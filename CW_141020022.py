ced091df51f8c195df9b16a4488f2434a4c2f79a
yagupov <veny.2006@mail.ru>
2022-10-14 13:17:28 +0300

    Закончил

--- CW_141020022.py
+++ CW_141020022.py
@@ -1,2 +1,22 @@
 #Classwork 14.10.22
-print('Hello, world!')
+'''a = input('ABOBA:') # Сохраняется как str
+print(a)
+print(type(a)) # Вывод типа переменной а'''
+
+'''the_spisok = [123,'123'] # Создание списка
+print(the_spisok) # >>> [123,'123']
+print(the_spisok[1]) # >>> '123' / Индекс начинается с 0
+
+the_spisok.append(321) # Добавление элемента в список
+print(the_spisok) # >>> [123,'123',321]
+print(the_spisok[1:2]) # >>> ['123']
+
+print(the_spisok[2:0:-1]) # >>> [321,'123']'''
+
+the_slovar = {0: "The zero", 1:"Sweet h Ala,", 2:"Paint it black"}
+print(the_slovar[2])
+the_2nd_slovar = {"City": "Zelek",
+                  "District": 14,
+                  "Build": 1435}
+print(the_2nd_slovar) # >>> {'City': 'Zelenograd', 'District':14)
+print(the_2nd_slovar["District"])
