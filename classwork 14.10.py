388395404fd1d6cc7cc20a20971571c5b0b11ea4
parrkerrr <paket777775@mail.ru>
2022-10-14 13:16:47 +0300

    Закончил классную работу

--- classwork 14.10.py
+++ classwork 14.10.py
@@ -1,2 +1,22 @@
 #Classwork 14.10.2022
-print("Hello, world!")
+""""# Ввод с клавиатуры
+a = input("Enter some shit: ") #Сохраняется к st5r
+print(a)
+print(type(a)) # вывод типа переменной a"""
+
+
+the_spisok = [123, "123"] #Создание списка
+print(the_spisok)
+print(the_spisok[1])
+
+the_spisok.append(321)
+print(the_spisok)
+print(the_spisok[1:2])
+
+print(the_spisok[2:0:-1])
+
+the_slovar = {0: "The Zero",
+              1: "Sweet home Alabama",
+              2: "Paint it Black"}
+print(the_spisok)
+print
