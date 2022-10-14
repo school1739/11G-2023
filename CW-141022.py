c108bcef09183e669470b3bc39e767edcbdf98b3
kakoito-chel <herobrineredmi21011@gmail.com>
2022-10-14 13:21:05 +0300

    Клс.Раб 14,10,2022(1000-7)

---
+++ CW-141022.py
@@ -0,0 +1,35 @@
+# print("Хелоу ворлд!")#Classwork 14.10.22
+# a=input('Enter here some shit:')# Ввод с клавиатуры
+# print(a)
+# print(type(a))# ввод типа переменной
+
+
+# the_spisok = [123, '123']
+# print(the_spisok)
+# print(the_spisok[1])
+#
+# the_spisok.append(321)
+# print(the_spisok)
+# print(the_spisok[1:2])
+#
+# print(the_spisok[2:0:-1])
+#                         #Т.к. область опр. [с;]
+
+
+the_slovar = {0:"The Zero",
+              1:'Dungeon master',
+              2:'My name is bogdan, i like kakich'}
+print(the_slovar)
+print(the_slovar[0])
+
+adr = {'city': "Moskow, Zelenograd",
+       "street":"Pypkina zolupkina",
+       "Building": 1000-7}
+
+print(adr)
+print(adr["street"])
+
+Aboba={45:'The forty-five',
+       'Five': "V"}
+print (Aboba[45])
+print(Aboba['Five'])
