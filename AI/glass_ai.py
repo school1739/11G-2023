cd9dd946adceed380cc9bc85941100d84fad4441
Ronopoldo <Ronpoldo@gmail.com>
2022-10-14 13:20:01 +0300

    Крч изучили массивы и словари вау вау вау :scream:
    Классная работа за 10/14

--- AI/glass_ai.py
+++ AI/glass_ai.py
@@ -1,3 +1,32 @@
-# Classwork 22/10/14
+# # Classwork 22/10/14
+# print("ХЫЛОУ ВОРЛД!!11 :flushed: :flushed:")
 
-print("ХЫЛОУ ВОРЛД!!11 :flushed: :flushed:")
+
+# a = input('Введите немного shit:') #Ввод данных (без типа)
+# print(a)
+# print(type(a)) #Напечатать тип даннных
+
+# array = [123, '123']
+# print(array)
+# print(array[1])
+# array.append(321) #Добавить 1 элемент на позицию [length]
+# print(array)
+# print(array[1:2]) #Область определения [a,b)
+# print(array[2:0:-1] # {-1} = шаг)
+
+#Тип списки как JSON'ы, но тип я не уверен
+obj = {
+           0: 'Биг Тейсти <333',
+           1: 'Биг Мак',
+           2: 'Чикен Премьер'
+       }
+print(obj[1]) #Ключ указывается в квадратных скобках (вне зависимости от типа)
+
+obj2 = {
+            'city': 'Moscow, Zelenograd',
+            'disctict': 15,
+            'building': 1555
+        }
+print(obj2["building"])
+
+#ПРОДАМ ГАРАЖ 8 (800) 555 - 35 - 35
