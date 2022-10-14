fa72a391958d20db3ad7e3f787544e608e0bfd88
arzumanyan <emmi.arzumanyan@gmail.com>
2022-10-14 13:23:58 +0300

    classwork 14.10.22

--- classwork.py
+++ classwork.py
@@ -1,2 +1,38 @@
-#classwork 14.10.22
-print('Hello, World!')
+'''#Ввод с клавиатуры
+a = input("Enter some shit:")
+print(a)
+print(type(a)) #Ввод типа переменной a
+'''
+
+'''the_spisok = [123,"123"] #Создание списка
+                         #массивы данных разных типов
+print(the_spisok) # >>> [123,'123']
+print(the_spisok[1]) # >>> '123' / Индекс начинается с 0
+
+the_spisok.append(321) # Добавление элементов в список
+print(the_spisok) # >>> [123, '123', 321]
+print(the_spisok[1:2]) # >>> ['123']
+                       #Тк область определения [a; b)
+
+print(the_spisok[2:0:-1]) # >>> [321, '123']
+                          #Тк область определения [c; a)'''
+
+   the_slovar {0: "The Zero"
+               1: "Sweet home Alabama"
+               2: "Paint it Black"}
+print(the_slovar)
+print(the_slovar[5])
+the_2nd_slovar={"City": "Muhosransk"
+               "District": 666,
+               "Building:": Domsumashedshego}
+print(the_2nd_slovar)#>>> {"City": "Muhosransk", "District": 666,
+               # 'Building:': Domsumashedshego}
+print(the_2nd_slovar["District"]) # >>> 666
+the_3nd_slovar={45:"The fourty five",
+                "Five": "V"}
+print(the_3nd_slovar[45]) # >>> The forty-five
+print(the_3nd_slovar["Five"]) # >>> V
+
+
+
+
