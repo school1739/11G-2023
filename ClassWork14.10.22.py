d0d906fb7398d1cbb83cab1f8396896e31e0ca8d
Бычков Лыгач <alekcey2132@mail.ru>
2022-10-14 13:18:16 +0300

    Classwork finished 14.10.22

--- ClassWork14.10.22.py
+++ ClassWork14.10.22.py
@@ -1,2 +1,33 @@
 #Classwork 14.10.22
-print("Hello, World!")
+
+# Ввод с клавиатцры
+"""a = input("Enter some shit")
+print(a)
+print(type(a))"""
+
+#Списки
+"""the_spisok= [123, "123"] #Создание списка/первое 123 - число, второе - строка
+print(the_spisok)
+print(the_spisok[1]) #индекс начинается с 0
+the_spisok.append(321) #Добавлениe элемента в список
+print(the_spisok)
+print(the_spisok[1:2])#>>> ['123']
+                      #Т.к область определения [a; b)
+print(the_spisok[2:0:-1])# >>> [321, '123']
+                         #Тюк область определения [c; a)"""
+
+#Словарь
+'''the_slovar = {5: "The Zero",
+              1: "Swet home Alabama",
+              2: "Paint it Black"}
+print(the_slovar) #>>> print(the_slower)
+print(the_slovar[5] #>>> The Zero
+the_2nd_slovar = {"City": "Zelenograd",
+                  "District": 14,
+                  "Building": 1435}
+print(the_2nd_slovar) #>>> {'City': 'Zelenograd','District': 14,'Building': 1435}
+print(the_2nd_slovar["Distict"]) #>>> 14
+the_3nd_slovar={45: "The fortu-five",
+                "Five": "V"}
+print(the_3nd_slovar[45])#>>> The forty-five
+print(the_3nd_slovar["Five"]) #>>> V'''
