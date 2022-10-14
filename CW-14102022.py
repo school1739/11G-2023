72d4f7183d51538239c6bbf3973cb3315f29f578
lishankov <Anton17_00@mail.ru>
2022-10-14 13:17:26 +0300

    ClassWork 14 10 2022 ClassWork done

--- CW-14102022.py
+++ CW-14102022.py
@@ -1,2 +1,35 @@
 # classwork 14.10.2022
-print("hello world")
+# Ввод с клавиатуры
+"""a = input("Enter some passages") # default у input - str
+print(a)
+print(type(a)) # Вывод типа переменной а"""
+"""the_spisok = [123, "123"] # создание списка
+print(the_spisok)
+
+print(the_spisok[1]) # >>> "123" / Индекс начинается с 0
+
+the_spisok.append(321) # Добавление элемента в список
+
+print(the_spisok[1:2]) # >>> ["123"] т.к. область определения [a, b)
+
+print(the_spisok[2:0:-1]) # >>> [321, "123"] т.к. область определения [0,2) [b, a)"""
+
+the_slovar = {5: 'The Null',
+              1:"Sweet home Alabama",
+              2: "We need no education"}
+print(the_slovar) # the slovar full
+
+print(the_slovar[5]) # The Null
+the_2nd_slovar = {"City": "Zelenograd",
+                  "District": 14,
+                  "Building": 1435}
+
+print(the_2nd_slovar) # full the_2nd_slovar
+
+print(the_2nd_slovar["District"]) # >>> 14
+
+the_3rd_slovar = {45: "The forty-five",
+                  "Five": "V"}
+
+print(the_3rd_slovar[45]) # >>> The forty-five
+print(the_3rd_slovar["Five"]) # >>> V
