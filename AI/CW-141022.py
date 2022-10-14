7a9c2e1a951565bf06b91e83ab2a1a83d191cf96
Xcx9 <KuzVL2006@yandex.ru>
2022-10-14 13:17:21 +0300

    Classwork finished.

--- AI/CW-141022.py
+++ AI/CW-141022.py
@@ -1,2 +1,32 @@
-# Classwork 14.10.22
-print("Hello, world!")
+'''# Ввод с клавиатуры
+a = input("Enter something:") # Сохраняется как str
+print(a)
+print(type(a)) # Вывод типа переменной а'''
+
+"""the_spisok = [123, "123"] # Создание  списка
+                            # (массива данных разных типов)
+print(the_spisok) # >>> [123, '123']
+print(the_spisok[1]) # >>> '123' / индекс начинается с 0
+
+the_spisok.append(321) # добавление элемента в список
+print(the_spisok) # >>> [123, '123', 321]
+print(the_spisok[1:2]) # >>> ['123']
+                        # Т.к. область определения [a;b)
+
+print(the_spisok[2:0:-1]) # >>> [321, '123')
+                          # Т.к. область определения [c;b)"""
+
+"""the_slovar = {0: "the Zero",
+              1: "Sweet home Alabama",
+              2: "Paint in Black",}
+print(the_slovar) # 
+print(the_slovar[2]) # The Zero
+the_2nd_slovar = {"City": "Zelenograd",
+                  "District": 14,
+                  "Building:": 1545}
+print(the_2nd_slovar) # >>> {"City": "Zelenograd", "District": 14, "Building:": 1545}
+print(the_2nd_slovar["District"]) # >>> 14
+the_3rd_slovar{45: "The forty-five,",
+                  "Five": "V"}
+print(the_3rd_slovar[45]) # >>> The forty-five
+print(the_3rd_slovar["Five"]) # >>> V"""
