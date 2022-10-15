404e408a8fe79946376200f3e3fe8ce604b112e1
Женя Е <janeeg56@yandex.ru>
2022-10-15 23:13:05 +0300

    Jane E. Homework

--- egorova.py
+++ egorova.py
@@ -1,2 +1,13 @@
 print("Hello, world!")
 #Eгорова Евгения Сергеевна
+vocabulary imitating stakanov = {'stakanchikONE' : [1,2,1,2,1,2],
+                                 'stakanchikTWO' : [1,2,1,2,1,2],
+                                 'stakanchikTHREE' : [1,2,1,2,1,2],
+                                 'stakanchikFOUR' : [1,2,1,2,1,2],
+                                 'stakanchikFIVE' : [1,2,1,2,1,2],
+                                 'stakanchikSIX' : [1,2,1,2,1,2],
+                                 'stakanchikSEVEN' : [1,2,1,2,1,2],
+                                 'stakanchikEIGHT' : [1,2,1,2,1,2],
+                                 'stakanchikNINE' : [1,2,1,2,1,2],
+                                 'stakanchikTEN' : [1,2,1,2,1,2],
+                                 'stakanchikELEVEN' : [1,2,1,2,1,2]}
