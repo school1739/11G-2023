bfaf7f319809c33da3fcdb4f3811197a3c838bf4
Бычков <alekcey2132@mail.ru>
2022-10-16 23:40:32 +0300

    homework 16.10.2022 bychkov

--- Ai/glass_Ai.py
+++ Ai/glass_Ai.py
@@ -1,1 +1,34 @@
-print("Hello, World!")
+import random
+spisok1 = [random.randint(1, 2), random.randint(1, 2), random.randint(1, 2)]
+spisok2 = [random.randint(1, 2), random.randint(1, 2), random.randint(1, 2)]
+spisok3 = [random.randint(1, 2), random.randint(1, 2), random.randint(1, 2)]
+spisok4 = [random.randint(1, 2), random.randint(1, 2), random.randint(1, 2)]
+spisok5 = [random.randint(1, 2), random.randint(1, 2), random.randint(1, 2)]
+spisok6 = [random.randint(1, 2), random.randint(1, 2), random.randint(1, 2)]
+spisok7 = [random.randint(1, 2), random.randint(1, 2), random.randint(1, 2)]
+spisok8 = [random.randint(1, 2), random.randint(1, 2), random.randint(1, 2)]
+spisok9 = [random.randint(1, 2), random.randint(1, 2), random.randint(1, 2)]
+spisok10 = [random.randint(1, 2), random.randint(1, 2), random.randint(1, 2)]
+spisok11 = [random.randint(1, 2), random.randint(1, 2), random.randint(1, 2)]
+the_slovar={"Стакан 1": spisok1,
+            "Стакан 2": spisok2,
+            "Стакан 3": spisok3,
+            "Стакан 4": spisok4,
+            "Стакан 5": spisok5,
+            "Стакан 6": spisok6,
+            "Стакан 7": spisok7,
+            "Стакан 8": spisok8,
+            "Стакан 9": spisok9,
+            "Стакан 10": spisok10,
+            "Стакан 11": spisok11,}
+print(the_slovar["Стакан 1"])
+print(the_slovar["Стакан 2"])
+print(the_slovar["Стакан 3"])
+print(the_slovar["Стакан 4"])
+print(the_slovar["Стакан 5"])
+print(the_slovar["Стакан 6"])
+print(the_slovar["Стакан 7"])
+print(the_slovar["Стакан 8"])
+print(the_slovar["Стакан 9"])
+print(the_slovar["Стакан 10"])
+print(the_slovar["Стакан 11"])
--- bychkov.py
+++ bychkov.py
@@ -1,3 +1,17 @@
-print('Hello, World!')
+'''print('Hello, World!')
 
-#Бычков Алексей Витальевич
+#Бычков Алексей Витальевич'''
+import random
+sp = [random.randint(1, 2), random.randint(1, 2), random.randint(1, 2)]
+the_slovar={"Стакан 1": sp,
+            "Стакан 2": sp,
+            "Стакан 3": sp,
+            "Стакан 4": sp,
+            "Стакан 5": sp,
+            "Стакан 6": sp,
+            "Стакан 7": sp,
+            "Стакан 8": sp,
+            "Стакан 9": sp,
+            "Стакан 10": sp,
+            "Стакан 11": sp,
+            }
