2aee231bf7f2c439f2ee70c1d2e48a0356e84e5c
Shlepabot <mail45@mail.ru>
2023-02-01 14:27:22 +0300

    End of Classwork

--- Info_sec/Ave_Caesar.py
+++ Info_sec/Ave_Caesar.py
@@ -1,14 +1,19 @@
+# Базанов, Кузнецов В., Егорова
 RUS_ALPHA = 'АБВГДЕЁЖЗЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
 ENG_ALPHA = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 offset = int(input('Введите сдвиг: '))
-offset = offset % len(RUS_ALPHA)
+# offset = offset % len(RUS_ALPHA)
 message = input('Введите сообщение: ').upper()
 answer = ''
-
+if offset > len(RUS_ALPHA):
+    n = offset // len(RUS_ALPHA)
+    offset = offset - n * len(RUS_ALPHA)
 for char in message:
     char_place = RUS_ALPHA.find(char)
     new_place = char_place + offset
+    if new_place > len(RUS_ALPHA):
+        new_place = new_place - len(RUS_ALPHA)
     if char in RUS_ALPHA:
         answer += RUS_ALPHA[new_place]
     elif char in ENG_ALPHA:
