dc63b5ab95b56e2b81a80a084a5c6df466c2536c
Bebra Bebrovich <bebra@bebra.com>
2023-02-01 14:21:44 +0300

    шифр цезаря окончание

--- Infosec/AveCaesar.py
+++ Infosec/AveCaesar.py
@@ -4,13 +4,21 @@ ENG_ALPHA = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 offset = int(input('Введите сдвиг: '))
 offset = offset % len(RUS_ALPHA)
 message = input('Введите сообщение: ').upper()
+
 answer = ''
 for char in message:
-    char_place = RUS_ALPHA.find(char)
-    new_place = char_place + offset
     if char in RUS_ALPHA:
-        answer += RUS_ALPHA[new_place]
+        char_place = RUS_ALPHA.find(char)
+        new_place = char_place + offset
+        if char in RUS_ALPHA:
+            answer += RUS_ALPHA[new_place % len(RUS_ALPHA)]
+    elif char in ENG_ALPHA:
+        char_place = ENG_ALPHA.find(char)
+        new_place = char_place + offset
+        if char in ENG_ALPHA:
+            answer += ENG_ALPHA[new_place % len(ENG_ALPHA)]
     else:
         answer += char
+        
 
 print(answer)
