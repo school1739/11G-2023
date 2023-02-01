92955c0ab458476d54dc0a5f473ad6ebb8a802f3
parrkerrr <paket777775@mail.ru>
2023-02-01 14:25:13 +0300

    шифр цезаря(upgrade)

--- infosek/Ave_Cesare.py
+++ infosek/Ave_Cesare.py
@@ -1,16 +1,23 @@
 RUS_ALPHA = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
-END_ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+ENG_ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 offset = int(input("Введите сдвиг: "))
-offset = offset % len(RUS_ALPHA)
 message = input("Введите сообщение: ").upper()
 answer = ""
 
 for char in message:
-    char_place = RUS_ALPHA.find(char)
-    new_place = char_place + offset
     if char in RUS_ALPHA:
-        answer += RUS_ALPHA[new_place]
-    else:
-        answer += char
+        char_place = RUS_ALPHA.find(char)
+        new_place = char_place + (offset % 33)
+        if char in RUS_ALPHA:
+            answer += RUS_ALPHA[new_place % 33]
+        else:
+            answer += char
+    if char in ENG_ALPHA:
+        char_place = ENG_ALPHA.find(char)
+        new_place = char_place + (offset % 26)
+        if char in ENG_ALPHA:
+            answer += ENG_ALPHA[new_place % 26]
+        else:
+            answer += char
 print(answer)
