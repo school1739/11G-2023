6d99f65b5f227352ae2791d89151579494cfc8cd
Bebra Bebrovich <Bebra@bebra.com>
2023-02-01 14:10:40 +0300

    шифр цезаря начало

---
+++ Infosec/AveCaesar.py
@@ -0,0 +1,16 @@
+RUS_ALPHA = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+ENG_ALPHA = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+offset = int(input('Введите сдвиг: '))
+offset = offset % len(RUS_ALPHA)
+message = input('Введите сообщение: ').upper()
+answer = ''
+for char in message:
+    char_place = RUS_ALPHA.find(char)
+    new_place = char_place + offset
+    if char in RUS_ALPHA:
+        answer += RUS_ALPHA[new_place]
+    else:
+        answer += char
+
+print(answer)
