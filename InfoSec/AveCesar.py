4feefed77cae8f304717b241775c2345bf24e8b6
Vladislav_sarkisan <for.programming.77@gmail.com>
2023-02-01 14:28:48 +0300

    дописали smthing shitfr (Миронова, Жарова, Хохлова)

---
+++ InfoSec/AveCesar.py
@@ -0,0 +1,25 @@
+RUS_ALPHA = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+ENG_ALPHA = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+offset = int(input('Введите сдвиг: '))
+offset = offset % len(RUS_ALPHA)
+message = input('Введите сообщение: ').upper()
+answer = ''
+
+for char in message:
+    if char in RUS_ALPHA:
+        char_place = RUS_ALPHA.find(char)
+        new_place = char_place + (offset % 33)
+        if char in RUS_ALPHA:
+            answer += RUS_ALPHA[new_place % 33]
+        else:
+            answer += char
+    if char in ENG_ALPHA:
+        char_place = ENG_ALPHA.find(char)
+        new_place = char_place + (offset % 26)
+        if char in ENG_ALPHA:
+            answer += ENG_ALPHA[new_place % 26]
+        else:
+            answer += char
+
+print(answer)
