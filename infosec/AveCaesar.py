1830e04ef04ba75c5179539d6e579ba350c71367
Student <Leonid.2k17@gmail.com>
2023-02-01 14:24:17 +0300

    Шифр Цезаря законченный (Токаренко Зубков)

--- infosec/AveCaesar.py
+++ infosec/AveCaesar.py
@@ -1,15 +1,19 @@
 RUS_ALPHA = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
 ENG_ALPHA = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-offset = int(input('Введите сдвиг: ')) % 33
+offset = int(input('Введите сдвиг: '))
 message = input('Введите сообщение: ').upper()
 answer = ''
 
 for char in message:
-    char_place = RUS_ALPHA.find(char)
-    new_place = char_place + offset
     if char in RUS_ALPHA:
-        answer += RUS_ALPHA[new_place]
+        char_place = RUS_ALPHA.find(char)
+        new_place = char_place + (offset % 33)
+        answer += RUS_ALPHA[new_place % 33]
+    elif char in ENG_ALPHA:
+        char_place = ENG_ALPHA.find(char)
+        new_place = char_place + (offset % 26)
+        answer += ENG_ALPHA[new_place % 26]
     else:
         answer += char
 
