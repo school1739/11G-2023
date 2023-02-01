5436a37fc2eb80d212f313029b0d7a37fa37e710
LinexForte <uh>
2023-02-01 14:23:43 +0300

    CW2 01.02.2023 Goncharov Sharapov

--- InfoSec/AveCaeser.py
+++ InfoSec/AveCaeser.py
@@ -1,11 +1,13 @@
 Alphabet_RU = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
 Alphabet_EN = "ABCDEFGHIKLMNOPQRSTVXYZ"
 message = input("Введите сообщение : ").upper()
-offset = (int(input("Введите сдвиг : ")) % len(Alphabet_RU) - 1)
+offset = int(input("Введите сдвиг : "))
 answer = ""
 for char in message:
     if char in Alphabet_RU:
-        answer += Alphabet_RU[Alphabet_RU.find(char) + offset]
+        answer += Alphabet_RU[(Alphabet_RU.find(char) + offset) % len(Alphabet_RU)]
+    elif char in Alphabet_EN:
+        answer += Alphabet_EN[(Alphabet_EN.find(char) + offset) % len(Alphabet_EN)]
     else:
         answer += char
 print(answer)
