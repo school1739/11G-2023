f0534f752ca79071aabbdaf09fdcd42d6cdbdbc7
Bormotoon <bormotoon@gmail.com>
2023-02-02 11:16:54 +0300

    Ver. 0.2. BF not working... yet.

--- InfoSec/AveCaesar.py
+++ InfoSec/AveCaesar.py
@@ -1,17 +1,43 @@
+mode_choice = int(input("Выберите режим работы: 1 - шифрование, 2 - дешифрование, 3 - bruteforce: "))
 RUS_ALPHA = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
 ENG_ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-offset = int(input("Введите сдвиг: "))
-offset = offset % len(RUS_ALPHA)
-message = input("Введите сообщение: ").upper()
+text = input("Введите текст: ").upper()
 answer = ""
 
-for char in message:
-    char_place = RUS_ALPHA.find(char)
-    new_place = char_place + offset
-    if char in RUS_ALPHA:
-        answer += RUS_ALPHA[new_place]
-    else:
-        answer += char
 
-print(answer)
+def iterate_text(text, mode):
+    global answer
+    match mode:
+        case "encrypt":
+            op = "+ offset"
+        case "decrypt":
+            op = "- offset"
+        case "bruteforce":
+            op = "- i"
+    for letter in text:
+        if letter in RUS_ALPHA:
+            answer += RUS_ALPHA[eval("RUS_ALPHA.index(letter)" + op + "% len(RUS_ALPHA)")]
+        elif letter in ENG_ALPHA:
+            answer += ENG_ALPHA[eval("(ENG_ALPHA.index(letter)" + op + "% len(ENG_ALPHA)")]
+        else:
+            answer += letter
+
+
+match mode_choice:
+    case 1:
+        offset = int(input("Введите сдвиг: "))
+        iterate_text(text, "encrypt")
+        print(answer)
+
+    case 2:
+        offset = int(input("Введите сдвиг: "))
+        iterate_text(text, "decrypt")
+        print(answer)
+
+    case 3:
+        iterate_text(text, "bruteforce")
+        print(answer)
+
+    case _:
+        print("Неверный режим работы")
