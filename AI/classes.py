e28e48700fbcb5b08c5f191dd8acb588f9293587
Student <veny.2006@mail.ru>
2023-02-13 12:02:11 +0300

    classwork 13.02

---
+++ AI/classes.py
@@ -0,0 +1,13 @@
+# pep8
+# Переменные some_peremennaya
+# Классы: SomeClass
+# Функции: some_function
+
+class Human:
+    pass
+
+rick = Human()
+nick = Human()
+dick = Human()
+
+print(rick, nick, dick)
