51f0f252de3b1a665d197d94666dbbcc32eee97c
Maksimkaggg <114922584+Maksimkaggg@users.noreply.github.com>
2023-02-13 11:57:38 +0300

    Стаканы

--- AI/glass_AI.py
+++ AI/glass_AI.py
@@ -1,3 +1,89 @@
-print ('Hello world')
+import random # импорт библиотеки случайных чисел
 
+# Создаём словарь стаканов
+the_glasses={1:[1,1,1,1,1,2,2,2,2,2],
+             2:[1,1,1,1,1,2,2,2,2,2],
+             3:[1,1,1,1,1,2,2,2,2,2],
+             4:[1,1,1,1,1,2,2,2,2,2],
+             5:[1,1,1,1,1,2,2,2,2,2],
+             6:[1,1,1,1,1,2,2,2,2,2],
+             7:[1,1,1,1,1,2,2,2,2,2],
+             8:[1,1,1,1,1,2,2,2,2,2],
+             9:[1,1,1,1,1,2,2,2,2,2],
+             10:[1,1,1,1,1,2,2,2,2,2],
+             11:[1,1,1,1,1,2,2,2,2,2]}
+used_glasses={}  # вывод словаря стаканов
 
+def aiTurn(situation):
+  print("Ходит ИИ:")
+  move = 0
+  if situation == 1:
+    move = 1
+  else:
+    if len(the_glasses[situation]) == 0:
+      move = random.choice([1, 2])
+    else:
+      move = random.choice(the_glasses[situation])
+  print(f"ИИ выбрал : {move}")
+  used_glasses.update({situation: move})
+  return move
+
+def humanTurn(situation):
+  print("Ходит Человек:")
+  move = 0
+  if situation == 1:
+    move = 1
+  else:
+    move = input("Выберите число 1 или 2 : ")
+    while move != "1" and move != "2":
+      move = input("Некорректный ввод. Выберите число 1 или 2 : ")
+  print(f"Человек выбрал : {int(move)}")
+  return int(move)
+
+def glassUpdate(aiLastWin):
+    if aiLastWin == True:
+      for key in used_glasses:
+        the_glasses[key].append(used_glasses[key])
+    else:
+      for key in used_glasses:
+        if used_glasses[key] in the_glasses[key]:
+          the_glasses[key].remove(used_glasses[key])
+    return the_glasses
+
+def main(wins):
+  aiWins = 0
+  humanWins = 0
+  aiLastWin = False
+  used_glasses = {}
+  while aiWins <= 10 and humanWins <= 10:
+    situation = 11
+    if aiLastWin == True:
+      print("-----------------------------")
+      print(f"На столе {situation} палочек.")
+      situation -= humanTurn(situation)
+    while situation >= 0:
+      if situation == 0:
+        print("человек проиграл")
+        aiLastWin = True
+        glassUpdate(aiLastWin)
+        aiWins += 1
+        break
+      else:
+        situation -= aiTurn(situation)
+      print("-----------------------------")
+      print(f"На столе {situation} палочек.")
+      if situation == 0:
+        print("AI проиграл")
+        aiLastWin = False
+        glassUpdate(aiLastWin)
+        humanWins += 1
+        break
+      else:
+        situation -= humanTurn(situation)
+      print("-----------------------------")
+      print(f"На столе {situation} палочек.")
+    print("-----------------------------")
+    print(the_glasses)
+    print("-----------------------------")
+
+main(10)
