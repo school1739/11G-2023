5832cd106d9e40c5571514b268a1c79edece3c7c
Student <veny.2006@mail.ru>
2023-02-13 12:15:34 +0300

    classwork 13.02

--- AI/classes.py
+++ AI/classes.py
@@ -3,11 +3,22 @@
 # Классы: SomeClass
 # Функции: some_function
 
-class Human:
-    pass
+class Human:  # Объявление класса Human
+    def say_hello(self):   # описание метода классов
+        print("Hello!")    # self -- "СЕБЯ" -- кто исполняет (сам объект)
 
-rick = Human()
+    def say_this(self, message):          # В методы можно ззаписывать аргументы
+        print(message)    # как в обычную функцию
+
+    def hello(self):
+        self.say_this('Hello this shit!')
+
+rick = Human()   # Создание объекта (сущности) класса Human
 nick = Human()
 dick = Human()
 
 print(rick, nick, dick)
+
+rick.say_hello()
+nick.say_this('Hello yourself!')
+dick.hello()
