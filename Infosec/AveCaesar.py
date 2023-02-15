ce375709d31c844ca5a1f9079bf75a445f31b89c
parrkerrr <paket777775@mail.ru>
2023-02-15 13:19:38 +0300

    decrypt bruteforce

--- Infosec/AveCaesar.py
+++ Infosec/AveCaesar.py
@@ -1,24 +1,64 @@
 RUS_ALPHA = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
 ENG_ALPHA = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-offset = int(input('Введите сдвиг: '))
-offset = offset % len(RUS_ALPHA)
+file_to_write = open("CaesarBebra.txt", "w", encoding='utf-8')
+mode = int(input('Введите режим: '))
+while str(mode) not in '123':
+    mode = int(input())
 message = input('Введите сообщение: ').upper()
-
-answer = ''
-for char in message:
-    if char in RUS_ALPHA:
-        char_place = RUS_ALPHA.find(char)
-        new_place = char_place + offset
+offset = int(input('Введите сдвиг: '))
+def encrypt(offset, message):
+    offset = int(offset)
+    offset = offset % len(RUS_ALPHA)
+    answer = ''
+    for char in message:
         if char in RUS_ALPHA:
-            answer += RUS_ALPHA[new_place % len(RUS_ALPHA)]
-    elif char in ENG_ALPHA:
-        char_place = ENG_ALPHA.find(char)
-        new_place = char_place + offset
-        if char in ENG_ALPHA:
-            answer += ENG_ALPHA[new_place % len(ENG_ALPHA)]
-    else:
-        answer += char
-        
+            char_place = RUS_ALPHA.find(char)
+            new_place = char_place + offset
+            if char in RUS_ALPHA:
+                answer += RUS_ALPHA[new_place % len(RUS_ALPHA)]
+        elif char in ENG_ALPHA:
+            char_place = ENG_ALPHA.find(char)
+            new_place = char_place + offset
+            if char in ENG_ALPHA:
+                answer += ENG_ALPHA[new_place % len(ENG_ALPHA)]
+        else:
+            answer += char
+    print(f'Сдвиг {offset}: {answer}')
+    file_to_write.write(answer)
 
-print(answer)
+def decrypt(offset, message):
+    offset = int(offset)
+    offset = offset % len(RUS_ALPHA)
+
+    answer = ''
+    for char in message:
+        if char in RUS_ALPHA:
+            char_place = RUS_ALPHA.find(char)
+            new_place = char_place - offset
+            if char in RUS_ALPHA:
+                answer += RUS_ALPHA[new_place % len(RUS_ALPHA)]
+        elif char in ENG_ALPHA:
+            char_place = ENG_ALPHA.find(char)
+            new_place = char_place - offset
+            if char in ENG_ALPHA:
+                answer += ENG_ALPHA[new_place % len(ENG_ALPHA)]
+        else:
+            answer += char
+    print(answer)
+    file_to_write.write(answer)
+
+def bruteforce(message):
+    for i in range(1, 32):
+        encrypt(i, message)
+
+
+match mode:
+     case 1:
+         encrypt(offset, message)
+     case 2:
+         decrypt(offset, message)
+     case 3:
+         bruteforce(message)
+     case _:
+         print("Нет такого режима")
