e05e0eaedccbdbe28a43fe707519e7ec63ede97e
Student <VFBNM>
2023-02-15 13:04:49 +0300

    classwork 15.02

--- infosec/AveCaesar.py
+++ infosec/AveCaesar.py
@@ -1,20 +1,58 @@
 RUS_ALPHA = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
 ENG_ALPHA = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-offset = int(input('Введите сдвиг: '))
+made_choice = int(input('Выберите операцию:'
+                        ' 1 - шифрование'
+                        ' 2 - дешифровка'
+                        ' 3 - перебор'))
+
 message = input('Введите сообщение: ').upper()
 answer = ''
+nomer = 0
+if made_choice == 1:
+    offset = int(input('Введите сдвиг: '))
+    for char in message:
+        if char in RUS_ALPHA:
+            char_place = RUS_ALPHA.find(char)
+            new_place = char_place + (offset % 33)
+            answer += RUS_ALPHA[new_place % 33]
+        elif char in ENG_ALPHA:
+            char_place = ENG_ALPHA.find(char)
+            new_place = char_place + (offset % 26)
+            answer += ENG_ALPHA[new_place % 26]
+        else:
+            answer += char
+    print(answer)
 
-for char in message:
-    if char in RUS_ALPHA:
-        char_place = RUS_ALPHA.find(char)
-        new_place = char_place + (offset % 33)
-        answer += RUS_ALPHA[new_place % 33]
-    elif char in ENG_ALPHA:
-        char_place = ENG_ALPHA.find(char)
-        new_place = char_place + (offset % 26)
-        answer += ENG_ALPHA[new_place % 26]
-    else:
-        answer += char
+if made_choice == 2:
+    offset = int(input('Введите сдвиг: '))
+    for char in message:
+        if char in RUS_ALPHA:
+            char_place = RUS_ALPHA.find(char)
+            new_place = char_place - (offset % 33)
+            answer += RUS_ALPHA[new_place % 33]
+        elif char in ENG_ALPHA:
+            char_place = ENG_ALPHA.find(char)
+            new_place = char_place - (offset % 26)
+            answer += ENG_ALPHA[new_place % 26]
+        else:
+            answer += char
+    print(answer)
 
-print(answer)
+if made_choice == 3:
+    for offset in range(32):
+        answer = ''
+        nomer += 1
+        for char in message:
+            if char in RUS_ALPHA:
+                char_place = RUS_ALPHA.find(char)
+                new_place = char_place - (offset % 33)
+                answer += RUS_ALPHA[new_place % 33]
+            elif char in ENG_ALPHA:
+                char_place = ENG_ALPHA.find(char)
+                new_place = char_place - (offset % 26)
+                answer += ENG_ALPHA[new_place % 26]
+            else:
+                answer += char
+        print(nomer, answer)
+
