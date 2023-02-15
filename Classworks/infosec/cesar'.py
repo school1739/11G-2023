7afd1aba0a4c0e912b55522f53c3b1d029724212
Vladislav_sarkisan <for.programming.77@gmail.com>
2023-02-15 14:25:00 +0300

    15.02/2

--- Classworks/infosec/cesar'.py
+++ Classworks/infosec/cesar'.py
@@ -1,5 +1,31 @@
+from ftplib import FTP
+
 Alphabet_RU = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
 Alphabet_EN = "ABCDEFGHIKLMNOPQRSTVXYZ"
+
+HOST = 'vh388.timeweb.ru'
+PORT = 21
+USER = 'bormotoon_infosec'
+PASSWORD = 'zfyLKkD3'
+
+ftp_server = FTP(HOST, USER, PASSWORD)
+filename = "CaesarLog.txt"
+
+with open(filename, "rb") as file:
+    ftp_server.storbinary(f"STOR {filename}", file)
+
+with open(filename, "wb") as file:
+    ftp_server.retrbinary(f"RETR {filename}", file.write)
+
+file = open(filename, "r")
+print('File Content:', file.read())
+ftp_server.quit()
+#Открываем файл на запись кодировка utf-8
+#аргументы: имя файла, режим работы, кодировка
+#Режим работы:
+    #r-read(только чтение)
+    #w-write(только запись)
+    #a-append(добавл в конец)
 fileToWrite=open("CaesarLog.txt", "w", encoding="utf-8")
 def main():
     message = input("Введите сообщение : ").upper()
@@ -9,22 +35,23 @@ def main():
     choice = int(input("Выберите режим : "))
     match choice:
         case 1:
-            print(encrypt(message, int(input("Сдвиг : "))))
+            encrypt(message, int(input("Сдвиг : ")))
         case 2:
-            print(decrypt(message, int(input("Сдвиг : "))))
+            decrypt(message, int(input("Сдвиг : ")))
         case 3:
             bruteforce(message)
         case _:
             print("Ошибка ввода")
 def encrypt(message, offset):
-    answer = ""    for char in message:
+    answer = ""
+    for char in message:
         if char in Alphabet_RU:
             answer += Alphabet_RU[(Alphabet_RU.find(char) + offset) % len(Alphabet_RU)]
         elif char in Alphabet_EN:
             answer += Alphabet_EN[(Alphabet_EN.find(char) + offset) % len(Alphabet_EN)]
         else:
             answer += char
-    return answer
+    print(answer)
 def decrypt(message, offset):
     answer = ""
     for char in message:
@@ -34,9 +61,9 @@ def decrypt(message, offset):
             answer += Alphabet_EN[(Alphabet_EN.find(char) - offset) % len(Alphabet_EN)]
         else:
             answer += char
-    return answer
+    print(answer)
 def bruteforce(message):
     for i in range(len(Alphabet_RU)):
-        fileToWrite.write(f"{i}: {encrypt(message, i)}\n")
+        encrypt(message, i)
 main()
 
