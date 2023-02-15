325ad01b96202725ff6f2c6b5b0c8a9dda459e06
misofia <78451488.sm@gmail.com>
2023-02-15 13:20:11 +0300

    запись в файл 15.02

--- InfoSec/AveCesar.py
+++ InfoSec/AveCesar.py
@@ -1,25 +1,63 @@
-RUS_ALPHA = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
-ENG_ALPHA = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+RUS_ALPHA = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+ENG_ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+answer = ""
 
-offset = int(input('Введите сдвиг: '))
-offset = offset % len(RUS_ALPHA)
-message = input('Введите сообщение: ').upper()
-answer = ''
+# Открыть файл
+file_to_write = open("CesarLog.txt", "w", encoding = "utf-8") # w - писать, r - читать
 
-for char in message:
-    if char in RUS_ALPHA:
-        char_place = RUS_ALPHA.find(char)
-        new_place = char_place + (offset % 33)
-        if char in RUS_ALPHA:
-            answer += RUS_ALPHA[new_place % 33]
+mode_choice = input("Выберите режим работы:\n"  # Ввод режима работы
+                    "1 - шифрование\n"
+                    "2 - дешифрование\n"
+                    "3 - bruteforce\n")
+while mode_choice not in "123":  # Защита от дурака
+    mode_choice = input("Неверный режим работы. Попробуйте ещё раз: ")
+mode_choice = int(mode_choice)
+
+text = input("Введите текст: ").upper()  # Ввод исходного сообщения
+
+
+def iterate_text(text, mode):  # Проход по символам исходного сообщения
+    global answer  # Будем записывать ответ в глобальную переменную
+    match mode:  # Выбор режима работы
+        case "encrypt":  # Шифрование
+            op = "+ offset"
+        case "decrypt":  # Дешифрование
+            op = "- offset"
+        case "bruteforce":  # Атака перебором
+            op = "- i"
+    for letter in text:  # Проход по символам
+        if letter in RUS_ALPHA:  # Если символ - русская буква
+            answer += RUS_ALPHA[eval(
+                "RUS_ALPHA.index(letter)" + op + "% len(RUS_ALPHA)")]  # Вычисляем индекс символа в алфавите и прибавляем сдвиг
+        elif letter in ENG_ALPHA:  # Если символ - английская буква
+            answer += ENG_ALPHA[eval(
+                "(ENG_ALPHA.index(letter)" + op + "% len(ENG_ALPHA)")]  # Вычисляем индекс символа в алфавите и прибавляем сдвиг
         else:
-            answer += char
-    if char in ENG_ALPHA:
-        char_place = ENG_ALPHA.find(char)
-        new_place = char_place + (offset % 26)
-        if char in ENG_ALPHA:
-            answer += ENG_ALPHA[new_place % 26]
-        else:
-            answer += char
+            answer += letter  # Если символ не буква, то просто добавляем его в ответ
 
-print(answer)
+
+match mode_choice:  # Включение режима работы, соответствующего выбранному пользователем
+    case 1:
+        offset = int(input("Введите сдвиг: "))
+        iterate_text(text, "encrypt")
+        print(answer)
+        file_to_write.write(answer) # Записать в файл
+
+    case 2:
+        offset = int(input("Введите сдвиг: "))
+        iterate_text(text, "decrypt")
+        print(answer)
+        file_to_write.write(answer) # Записать в файл
+
+    case 3:
+        for i in range(1, 32):
+            iterate_text(text, "bruteforce")
+            print(f"Сдвиг {i}: {answer}")
+            file_to_write.write(f"Сдвиг {i}: {answer}\n") # Записать в файл
+            answer = ""
+
+
+    case _: # Защита от дурака
+        print("Неверный режим работы")
+
+file_to_write.close() # Закрыть файл
