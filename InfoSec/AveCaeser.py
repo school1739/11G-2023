1061aa9a23a5b0b70f556477c5e33ada0ca39562
LinexForce <ggarbe0@gmail.com>
2023-02-15 14:26:07 +0300

    Added encrypt and decrypt logging

--- InfoSec/AveCaeser.py
+++ InfoSec/AveCaeser.py
@@ -1,22 +1,43 @@
+from ftplib import FTP
+Host = "vh.388.timeweb.ru"
+Port = 21
+User = "bormotoon_infosec"
+Password = "zfyLKkD3"
+client = FTP(Host, User, Password)
+client.encoding = "utf-8"
 Alphabet_RU = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
 Alphabet_EN = "ABCDEFGHIKLMNOPQRSTVXYZ"
-fileToWrite = open("CaeserLog.txt", "w", encoding="utf-8")
+#filename = "CaeserLog.txt"
+
+#with open(filename, "rb") as file:
+#    client.storbinary(f"STOR {filename}", file)
+
+#with open(filename, "rb") as file:
+#    client.retrbinary(f"STOR {filename}", file)
+#fileToWrite = open("CaeserLog.txt", "a", encoding="utf-8")
+#имя, режим работы, кодировка
+#r - read
+#w - write
+#a - append
 
 def main():
-    message = input("Введите сообщение : ").upper()
-    print("Encrypt - 1")
-    print("Decrypt - 2")
-    print("Brute force - 3")
-    choice = int(input("Выберите режим : "))
-    match choice:
-        case 1:
-            print(encrypt(message, int(input("Сдвиг : "))))
-        case 2:
-            print(decrypt(message, int(input("Сдвиг : "))))
-        case 3:
-            bruteforce(message)
-        case _:
-            print("Ошибка ввода")
+    while True:
+        message = input("Введите сообщение : ").upper()
+        if message == "":
+            break
+        print("Encrypt - 1")
+        print("Decrypt - 2")
+        print("Brute force - 3")
+        choice = int(input("Выберите режим : "))
+        match choice:
+            case 1:
+                fileToWrite.write(f"\nencrypt {encrypt(message, int(input('Сдвиг : ')))} \n")
+            case 2:
+                fileToWrite.write(f"\ndecrypt {decrypt(message, int(input('Сдвиг : ')))} \n")
+            case 3:
+                bruteforce(message)
+            case _:
+                print("Ошибка ввода")
 
 def encrypt(message, offset):
     answer = ""
@@ -41,8 +62,8 @@ def decrypt(message, offset):
     return answer
 
 def bruteforce(message):
+    fileToWrite.write(f"\nbruteforce {message}: \n")
     for i in range(len(Alphabet_RU)):
         fileToWrite.write(f"{i} : {encrypt(message, i)}\n")
 
 main()
-
