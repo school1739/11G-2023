45146a03f4f6f33d06389b47e58ddbd07c32d5eb
Xcx9 <KuzVL2006@yandex.ru>
2023-02-27 12:08:13 +0300

    classwork

--- AI/classes.py
+++ AI/classes.py
@@ -1,4 +1,4 @@
-# Базанов Александр и Кузнецов Владимир
+'''# Базанов Александр и Кузнецов Владимир
 
 # PEP8:
 # Переменные: some_peremen
@@ -24,4 +24,35 @@ print(rick, nick, dick)
 
 rick.say_hello()
 nick.say_this('Hello yourself!')
-dick.hello()
+dick.hello()'''
+
+
+# The Classes. Lesson 2
+
+
+# Конструктор
+class Cat:
+    def __init__(self, name):
+        self.__name = name  # Свойство ИМЯ для объекта класса Cat
+        self.__age = 0 # __ - знак икапсуляции
+
+    def set_age(self, age):
+        if 1 <= age <= 25:
+            self.age = age
+        else:
+            print("Ты лошара (С) Roni")
+
+    def display_info(self):
+        print(f"My name is {self.name}, my age is {self.age}.")
+
+
+simons_cat = Cat("Rick")
+print(simons_cat.name, simons_cat.age)
+print(type(simons_cat.name), type(simons_cat.age))
+
+simons_cat.family = "Simpsons"
+print(simons_cat.family)
+simons_cat.display_info()
+
+simons_cat.set_age(30)
+print(simons_cat.name, simons_cat.age)
