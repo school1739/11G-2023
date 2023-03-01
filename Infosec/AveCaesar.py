b9e5b8c8563f4c9e235deb861c4529b4c24cadf6
misofia <78451488.sm@gmail.com>
2023-03-01 13:20:54 +0300

    bebracaesar

--- Infosec/AveCaesar.py
+++ Infosec/AveCaesar.py
@@ -1,113 +1,99 @@
 from ftplib import FTP
-RUS_ALPHA = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
-ENG_ALPHA = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+RUS_ALPHA = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+ENG_ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+answer = ""
+
+
+
+
+mode_choice = input("Выберите режим работы:\n"  # Ввод режима работы
+                    "1 - шифрование\n"
+                    "2 - дешифрование\n"
+                    "3 - bruteforce\n"
+                    "4 - Отправить файл\n"
+                    "5 - Скачать файл\n")
+while mode_choice not in "12345":  # Защита от дурака
+    mode_choice = input("Неверный режим работы. Попробуйте ещё раз: ")
+mode_choice = int(mode_choice)
 
 HOST = "vh388.timeweb.ru"
 PORT = 21
 USER = "bormotoon_infosec"
 PASSWORD = "zfyLKkD3"
 
-ftp = FTP()
-ftp.connect(HOST, PORT)
-ftp.login(USER, PASSWORD)
 
-# Открываем файл на запись, кодировка utf-8
-# аргументы: имя файла, режим работы, кодировка
-# Режимы работы
-    # r - read (только чтение)
-    # w -write (запись)
-    # a - append (добавить в конец)
-
-mode = int(input('Введите режим: '))
-while str(mode) not in '123':
-    mode = int(input())
-
-ftp_mode = int(input('Введите режим FTP: '))
-while str(mode) not in '12':
-    mode = int(input())
-
-
-message = input('Введите сообщение: ').upper()
-offset = int(input('Введите сдвиг: '))
-def encrypt(offset, message, k, updo): # k=1, если используется в брутфорсе
-    offset = int(offset)
-    offset = offset % len(RUS_ALPHA)
-    answer = ''
-    for char in message:
-        if char in RUS_ALPHA:
-            char_place = RUS_ALPHA.find(char)
-            new_place = char_place + offset
-            if char in RUS_ALPHA:
-                answer += RUS_ALPHA[new_place % len(RUS_ALPHA)]
-        elif char in ENG_ALPHA:
-            char_place = ENG_ALPHA.find(char)
-            new_place = char_place + offset
-            if char in ENG_ALPHA:
-                answer += ENG_ALPHA[new_place % len(ENG_ALPHA)]
+def iterate_text(text, mode):  # Проход по символам исходного сообщения
+    global answer  # Будем записывать ответ в глобальную переменную
+    match mode:  # Выбор режима работы
+        case "encrypt":  # Шифрование
+            op = "+ offset"
+        case "decrypt":  # Дешифрование
+            op = "- offset"
+        case "bruteforce":  # Атака перебором
+            op = "- i"
+    for letter in text:  # Проход по символам
+        if letter in RUS_ALPHA:  # Если символ - русская буква
+            answer += RUS_ALPHA[eval("(RUS_ALPHA.index(letter)" + op + ')' "% len(RUS_ALPHA)")]  # Вычисляем индекс символа в алфавите и прибавляем сдвиг
+        elif letter in ENG_ALPHA:  # Если символ - английская буква
+            answer += ENG_ALPHA[eval("(ENG_ALPHA.index(letter)" + op + ')' "% len(ENG_ALPHA)")]  # Вычисляем индекс символа в алфавите и прибавляем сдвиг
         else:
-            answer += char
-
-    a = f'Сдвиг {offset}: {answer}\n'
-    print(f'Сдвиг {offset}: {answer}')
-    if k == 1 and updo == 1:
-        file_to_write = open("CaesarBebra.txt", "a", encoding='utf-8')
-        file_to_write.write(a) # Добавить в файл
-        ftp.storbinary(f"STOR CaesarBebra.txt", file_to_write)
-    elif updo == 2:
-        file_to_write = open("CaesarBebra.txt", "w", encoding='utf-8')
-        file_to_write.write(a)  # Запись в файл
-        ftp.storbinary(f"STOR CaesarBebra.txt", file_to_write)
-
-def decrypt(offset, message):
-    offset = int(offset)
-    offset = offset % len(RUS_ALPHA)
-
-    answer = ''
-    for char in message:
-        if char in RUS_ALPHA:
-            char_place = RUS_ALPHA.find(char)
-            new_place = char_place - offset
-            if char in RUS_ALPHA:
-                answer += RUS_ALPHA[new_place % len(RUS_ALPHA)]
-        elif char in ENG_ALPHA:
-            char_place = ENG_ALPHA.find(char)
-            new_place = char_place - offset
-            if char in ENG_ALPHA:
-                answer += ENG_ALPHA[new_place % len(ENG_ALPHA)]
-        else:
-            answer += char
-    print(answer)
-    file_to_write = open("CaesarBebra.txt", "w", encoding='utf-8')
-    file_to_write.write(answer)  # Запись в файл
-    file_to_write.close()
-
-def bruteforce(message):
-    for i in range(1, 32):
-        encrypt(i, message, 1)
+            answer += letter  # Если символ не буква, то просто добавляем его в ответ
 
 
-match mode:
-     case 1:
-         encrypt(offset, message, 0)
-         match ftp_mode:
-             case 1:
-                encrypt(offset, message, 0, 'up')
-             case 2:
-                encrypt(offset, message, 0, 'do')
-     case 2:
-         decrypt(offset, message)
-         match ftp_mode:
-             case 1:
-                 encrypt(offset, message, 0, 'up')
-             case 2:
-                 encrypt(offset, message, 0, 'do')
-     case 3:
-         bruteforce(message)
-         match ftp_mode:
-             case 1:
-                 encrypt(offset, message, 0, 'up')
-             case 2:
-                 encrypt(offset, message, 0, 'do')
-     case _:
-         print("Нет такого режима")
+match mode_choice:  # Включение режима работы, соответствующего выбранному пользователем
+    case 1:
+        file_to_write = open("Bebralavin.txt", "w", encoding="utf-8")
+        text = input("Введите текст: ").upper()  # Ввод исходного сообщения
+        offset = int(input("Введите сдвиг: "))
+        iterate_text(text, "encrypt")
+        print(answer)
+        file_to_write.write("Шифровка:")
+        file_to_write.write(text)
+        file_to_write.write(" - ")
+        file_to_write.write(answer)
+        file_to_write.close()
+    case 2:
+        file_to_write = open("Bebralavin.txt", "w", encoding="utf-8")
+        text = input("Введите текст: ").upper()  # Ввод исходного сообщения
+        offset = int(input("Введите сдвиг: "))
+        iterate_text(text, "decrypt")
+        print(answer)
+        file_to_write.write("Дешифровка:")
+        file_to_write.write(text)
+        file_to_write.write(" - ")
+        file_to_write.write(answer)
+        file_to_write.close()
+    case 3:
+        file_to_write = open("Bebralavin.txt", "w", encoding="utf-8")
+        text = input("Введите текст: ").upper()  # Ввод исходного сообщения
+        file_to_write.write("bruteforce:")
+        file_to_write.write(text)
+        file_to_write.write(" - ")
+        for i in range(1, 32):
+            iterate_text(text, "bruteforce")
+            print(f"Сдвиг {i}: {answer}")
+            file_to_write.write(f"{i} сдвиг:{answer}")
+            file_to_write.write(", ")
+            answer = ""
+        file_to_write.close()
+    case 4:
+        ftp = FTP()
+        ftp.connect(HOST, PORT)
+        ftp.login(USER, PASSWORD)
+        file_name = input("Какой файл отправить?\n")
+        ftp.storbinary('STOR ' + file_name, open(file_name, "rb"))  # rb - Чтение двоичного файла
+        ftp.quit()
+    case 5:
+        ftp = FTP()
+        ftp.connect(HOST, PORT)
+        ftp.login(USER, PASSWORD)
+        ftp.retrlines('LIST')
+        file_name = input("Выберите файл для скачивания:")
+        my_file = open(file_name, 'wb')  # wb - Запись двоичного файла
+        ftp.retrbinary('RETR ' + file_name, my_file.write, 1024)
+        ftp.quit()
+        my_file.close()
+    case _:  # Защита от дурака
+        print("Неверный режим работы")
 
