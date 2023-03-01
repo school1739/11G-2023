2054eb89b798f258f7243abeb310b10b19a9a38e
parrkerrr <paket777775@mail.ru>
2023-03-01 13:22:27 +0300

    Миронова Хохлова Жарова солнце светит девы пашут

--- InfoSec/AveCesar.py
+++ InfoSec/AveCesar.py
@@ -1,49 +1,23 @@
-import ftplib
 from ftplib import FTP
 
 RUS_ALPHA = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
 ENG_ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 answer = ""
 
+mode_choice = input("Выберите режим работы:\n"  # Ввод режима работы
+                    "1 - шифрование\n"
+                    "2 - дешифрование\n"
+                    "3 - bruteforce\n"
+                    "4 - Отправить файл\n"
+                    "5 - Скачать файл\n")
+while mode_choice not in "12345":  # Защита от дурака
+    mode_choice = input("Неверный режим работы. Попробуйте ещё раз: ")
+mode_choice = int(mode_choice)
 
 HOST = "vh388.timeweb.ru"
 PORT = 21
 USER = "bormotoon_infosec"
 PASSWORD = "zfyLKkD3"
-
-
-# Подключение к серверу
-ftp_server = FTP(HOST, USER, PASSWORD)
-filename = "CesarLog.txt"
-with open(filename, "rb") as file:
-    # Command for Uploading the file "STOR filename"
-    ftp_server.storbinary(f"STOR {filename}", file)
-
-with open(filename, "wb") as file:
-    # Command for Downloading the file "RETR filename"
-    ftp_server.retrbinary(f"RETR {filename}", file.write)
-
-file= open(filename, "r")
-print('File Content:', file.read())
-
-# Открыть файл на запись, кодировка UTF-8
-# Аргументы: имя файла, режим работы, кодировка
-# Режимы работы:
-    # r - read (только чтение)
-    # w - write (запись)
-    # a - append (добавить в конец)
-
-# file_to_write = open("CesarLog.txt", "w", encoding = "utf-8") # w - писать, r - читать
-
-mode_choice = input("Выберите режим работы:\n"  # Ввод режима работы
-                    "1 - шифрование\n"
-                    "2 - дешифрование\n"
-                    "3 - bruteforce\n")
-while mode_choice not in "123":  # Защита от дурака
-    mode_choice = input("Неверный режим работы. Попробуйте ещё раз: ")
-mode_choice = int(mode_choice)
-
-text = input("Введите текст: ").upper()  # Ввод исходного сообщения
 
 
 def iterate_text(text, mode):  # Проход по символам исходного сообщения
@@ -57,40 +31,66 @@ def iterate_text(text, mode):  # Проход по символам исходн
             op = "- i"
     for letter in text:  # Проход по символам
         if letter in RUS_ALPHA:  # Если символ - русская буква
-            answer += RUS_ALPHA[eval(
-                "RUS_ALPHA.index(letter)" + op + "% len(RUS_ALPHA)")]  # Вычисляем индекс символа в алфавите и прибавляем сдвиг
+            answer += RUS_ALPHA[eval("(RUS_ALPHA.index(letter)" + op + ')' "% len(RUS_ALPHA)")]  # Вычисляем индекс символа в алфавите и прибавляем сдвиг
         elif letter in ENG_ALPHA:  # Если символ - английская буква
-            answer += ENG_ALPHA[eval(
-                "(ENG_ALPHA.index(letter)" + op + "% len(ENG_ALPHA)")]  # Вычисляем индекс символа в алфавите и прибавляем сдвиг
+            answer += ENG_ALPHA[eval("(ENG_ALPHA.index(letter)" + op + ')' "% len(ENG_ALPHA)")]  # Вычисляем индекс символа в алфавите и прибавляем сдвиг
         else:
             answer += letter  # Если символ не буква, то просто добавляем его в ответ
 
 
 match mode_choice:  # Включение режима работы, соответствующего выбранному пользователем
     case 1:
+        file_to_write = open("CesarLog.txt", "w", encoding="utf-8")
+        text = input("Введите текст: ").upper()  # Ввод исходного сообщения
         offset = int(input("Введите сдвиг: "))
         iterate_text(text, "encrypt")
-        file_to_write = open("CesarLog.txt", "w", encoding="utf-8")
-        file_to_write.write(answer) # Записать в файл
-
+        print(answer)
+        file_to_write.write("Шифровка:")
+        file_to_write.write(text)
+        file_to_write.write(" - ")
+        file_to_write.write(answer)
+        file_to_write.close()
     case 2:
+        file_to_write = open("CesarLog.txt", "w", encoding="utf-8")
+        text = input("Введите текст: ").upper()  # Ввод исходного сообщения
         offset = int(input("Введите сдвиг: "))
         iterate_text(text, "decrypt")
         print(answer)
-        file_to_write = open("CesarLog.txt", "w", encoding="utf-8")
-        file_to_write.write(answer) # Записать в файл
-
+        file_to_write.write("Дешифровка:")
+        file_to_write.write(text)
+        file_to_write.write(" - ")
+        file_to_write.write(answer)
+        file_to_write.close()
     case 3:
+        file_to_write = open("CesarLog.txt", "w", encoding="utf-8")
+        text = input("Введите текст: ").upper()  # Ввод исходного сообщения
+        file_to_write.write("bruteforce:")
+        file_to_write.write(text)
+        file_to_write.write(" - ")
         for i in range(1, 32):
             iterate_text(text, "bruteforce")
             print(f"Сдвиг {i}: {answer}")
-            file_to_write = open("CesarLog.txt", "a", encoding="utf-8")
-            file_to_write.write(f"Сдвиг {i}: {answer}\n") # Записать в файл
+            file_to_write.write(f"{i} сдвиг:{answer}")
+            file_to_write.write(", ")
             answer = ""
-
-
-    case _: # Защита от дурака
+            file_to_write.close()
+    case 4:
+        ftp = FTP()
+        ftp.connect(HOST, PORT)
+        ftp.login(USER, PASSWORD)
+        # print(open())
+        file_name = input("Какой файл отправить?\n")
+        ftp.storbinary('STOR ' + file_name, open(file_name, "rb"))  # rb - Чтение двоичного файла
+        ftp.quit()
+    case 5:
+        ftp = FTP()
+        ftp.connect(HOST, PORT)
+        ftp.login(USER, PASSWORD)
+        ftp.retrlines('LIST')
+        file_name = input("Выберите файл для скачивания:")
+        my_file = open(file_name, 'wb')  # wb - Запись двоичного файла
+        ftp.retrbinary('RETR ' + file_name, my_file.write, 1024)
+        ftp.quit()
+        my_file.close()
+    case _:  # Защита от дурака
         print("Неверный режим работы")
-
-file_to_write.close() # Закрыть файл
-
