671f228c369216979f1a8731ee73999290525dce
Student <sdgas@gmail.com>
2023-03-07 15:22:36 +0300

    задание 12

--- informatika/codes.py
+++ informatika/codes.py
@@ -1,4 +1,5 @@
-from itertools import *
+#задание 2
+'''from itertools import *
 
 def f(x, y, z, w):
     return not(y <= x) or (z <= w) or not z
@@ -11,4 +12,14 @@ for a in product([0, 1], repeat=7):
     if len(table)== len(set(table)):
         for p in permutations('xyzw'):
             if [f(**dict(zip(p, r)))for r in table]==[0, 0, 0]:
-                print(p)
+                print(p)'''
+#задание 12
+s='1' + '8'*80
+while '18' in s or '288' in s or '3888' in s:
+    if '18' in s:
+        s=s.replace('18', '2', 1)
+    elif '288' in s:
+        s=s.replace('288', '3', 1)
+    else:
+        s=s.replace('3888', '1', 1)
+print(s)
