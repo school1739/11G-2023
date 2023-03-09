d3ca894671484242c697948401d859d37e0a6ad1
Student <lady.khodina@yandex.ru>
2023-03-09 13:24:28 +0300

    получилось

---
+++ BIG_data/matrix.py
@@ -0,0 +1,18 @@
+matrix = [[2, 4, 1, 1], [0, 2, 0, 0], [2, 1, 1, 3], [4, 0, 2, 3]]
+x = matrix[0][0]
+y =matrix[0][0]
+i = (x, y)
+def f(matrix, x, y):
+    temp = matrix[0].pop(0)
+   # for i in range(len(matrix)):
+
+        #matrix.remove(matrix[0][0])
+       # matrix.remove(matrix[0][0])
+
+
+    return matrix
+
+print(f(matrix, 0, 0))
+
+
+
