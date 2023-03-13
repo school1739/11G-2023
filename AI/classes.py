5eb5db9b80c39cef1b16bff6a72decd04d44d9ae
Student <ghjk>
2023-03-13 06:13:56 -0300

    classwork + start homework

--- AI/classes.py
+++ AI/classes.py
@@ -1,4 +1,4 @@
-class Human:  # Создаем новый класс
+"""class Human:  # Создаем новый класс
     def __init__(self, name):  # Конструктор
         print(f"Родился человек с именем {name}!")  # Сообщение о создании новой сущности
         self.__name = name  # ИНКАПСУЛИРОВАННЫЕ (__) свойства
@@ -26,10 +26,56 @@ class Student(Human):
 
 male_1 = Human("Rick")  # Создание сущности класса Human с именем Rick
 female_1 = Human("Doro")
-"""print(male_1.age())
-male_1.set_age(15)
-print(male_1.age())"""
+"""
+"""class Human:
+    def __init__(self, name):
+        self.name = name
 
-print(male_1.age)
-male_1.age = 15
-print(male_1.age)
+    @property  # Начало GETTER (получатель/возвращатель) СВОЙСТВА
+    def name(self):
+        return self.__name
+
+    @name.setter  # Начало SETTER ("установитель"/определитель) СВОЙСТВА
+    def name(self, name):
+        self.__name = name
+
+
+class Student(Human):
+    def __init__(self, name):
+        super().__init__(name)
+        self.__grade = 0
+
+    def study(self, univer):
+        print(f"{self.name} is in {univer}")
+
+    @property
+    def grade(self):
+        return self.__grade
+
+    @grade.setter
+    def grade(self, grade):
+        self.__grade = grade
+
+
+rick = Student("Rick")
+rick.study("MAI")
+print(rick.grade)
+rick.grade = 1
+print(rick.grade)
+"""
+
+
+class Neuron:
+    pass
+
+
+class S_Neuron(Neuron):
+    pass
+
+
+class A_Neuron(Neuron):
+    pass
+
+
+class R_Neuron(Neuron):
+    pass
