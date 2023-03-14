d766ff562d534bb8e6a8e516d7d4e61e0f4f1442
herob <herobrineredmi21011@gmail.com>
2023-03-15 01:26:00 +0300

    ап

--- MATRIX(2009)/Matrix spoooooooooooooooderman.py
+++ MATRIX(2009)/Matrix spoooooooooooooooderman.py
@@ -2,8 +2,25 @@ the_matrix=[['a11','a12','a13','a14'],
             ['a21','a22','a23','a24'],
             ['a31','a32','a33','a34'],
             ['a41','a42','a43','a44']]
-
-
+#
+#
+# def minor(input, i, j):
+#     output = []
+#     for counter in range(0, len(input)):
+#         output.append(input[counter])
+#         temp = output[counter].pop(j)
+#
+#     temp = output.pop(i)
+#
+#     return output
+# print(     'Матрица')
+# print('a11','a12','a13','a14')
+# print('a21','a22','a23','a24')
+# print('a31','a32','a33','a34')
+# print('a41','a42','a43','a44')
+# print("Найдем Минор")
+# i=int(input('Выберете число матрицы(Пример a11) :'))
+#
 def minor(input, i, j):
     output = []
     for counter in range(0, len(input)):
@@ -12,15 +29,18 @@ def minor(input, i, j):
 
     temp = output.pop(i)
 
+    for counter in range(len(output)):
+        output[counter].append(output[counter][0])
+        output[counter].append(output[counter][1])
+
+    for counter in range(3):
+
+    print(output)
     return output
-print(     'Матрица')
-print('a11','a12','a13','a14')
-print('a21','a22','a23','a24')
-print('a31','a32','a33','a34')
-print('a41','a42','a43','a44')
-print("Найдем Минор")
-ij=int(input('Выберете число матрицы(Пример a11) :'))
-if ij == a11:
-    print(minor(the_matrix, 0, 0))
-elif ij == a12:
-    print(minor(the_matrix, 0, 1))
+    # for counter in range(0, len(output)):
+    #   summ = summ + sum(output[counter])
+
+    # return(summ)
+
+
+print(minor(matrix, 1, 1))
