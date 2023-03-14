e943bd756e30d5b72418897fa0ddb9232da48ed7
Данила Зименский <zimenskiy17@gmail.com>
2023-03-15 00:40:36 +0300

    homework

--- AI/classes.py
+++ AI/classes.py
@@ -26,3 +26,65 @@ print(rick, nick,dick)
 rick.say_hello()
 nick.say_this()
 dick.hello()
+
+
+class Human: # Создаем новый класс
+    def __init__(self, name): # Конструктор
+        print(f'Родился человек с именем {name}!') # Сообщение о создании новой сущности
+        self.__name = name # инкапсулированные свойства
+        self.__age = 0
+        self.__health = 100
+        self.__height = 35
+
+    @property # Начало Getter (получатель/возвращатель)
+    def age(self):
+        return self.__age
+
+    @age.setter # Начало Setter (установитель) свойства
+    def age(self, new_age):
+        if 1<new_age<100:
+            self.__age = new_age
+        else:
+            print('No.')
+
+class Student(Human):
+    def school(self):
+        self.school = 1739
+        print()
+
+
+male_1 = Human('Rick') # Создание сущности класса Human с именем Rick
+female_1 = Human('Doro')
+
+
+class Human:
+    def __init__(self, name):
+        self.__name = name
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        self.__name = name
+
+class Student(Human):
+    def __init__(self, name):
+        super().__init__(name)
+        self.__grade = 0
+
+    def study(self, univer):
+        print(f'{self.name} studies in {univer}')
+
+    @property
+    def grade(self):
+        return self.__grade
+
+    @grade.setter
+    def grade(self, grade):
+        self.__grade = grade
+
+rick = Student('rick')
+rick.study('MAI')
+rick.grade += 1
---
+++ Infosec/AveCaesar.py
@@ -0,0 +1,121 @@
+from ftplib import FTP
+
+RUS_ALPHA = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+ENG_ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+answer = ""
+
+
+
+
+mode_choice = input("Выберите режим работы:\n"  # Ввод режима работы
+                    "1 - Шифрование\n"
+                    "2 - Дешифрование\n"
+                    "3 - Bruteforce\n"
+                    "4 - Отправить файл\n"
+                    "5 - Скачать файл\n"
+                    "6 - Список файлов\n"
+                    "7 - Выход\n")
+while mode_choice not in "1234567":  # Защита от дурака
+    mode_choice = input("Неверный режим работы. Попробуйте ещё раз: ")
+mode_choice = int(mode_choice)
+
+HOST = "vh388.timeweb.ru"
+PORT = 21
+USER = "bormotoon_infosec"
+PASSWORD = "zfyLKkD3"
+
+
+def iterate_text(text, mode):  # Проход по символам исходного сообщения
+    global answer  # Будем записывать ответ в глобальную переменную
+    match mode:  # Выбор режима работы
+        case "encrypt":  # Шифрование
+            op = "+ offset"
+        case "decrypt":  # Дешифрование
+            op = "- offset"
+        case "bruteforce":  # Атака перебором
+            op = "- i"
+    for letter in text:  # Проход по символам
+        if letter in RUS_ALPHA:  # Если символ - русская буква
+            answer += RUS_ALPHA[eval("(RUS_ALPHA.index(letter)" + op + ')' "% len(RUS_ALPHA)")]  # Вычисляем индекс символа в алфавите и прибавляем сдвиг
+        elif letter in ENG_ALPHA:  # Если символ - английская буква
+            answer += ENG_ALPHA[eval("(ENG_ALPHA.index(letter)" + op + ')' "% len(ENG_ALPHA)")]  # Вычисляем индекс символа в алфавите и прибавляем сдвиг
+        else:
+            answer += letter  # Если символ не буква, то просто добавляем его в ответ
+
+
+while mode_choice != 7:
+
+    match mode_choice:  # Включение режима работы, соответствующего выбранному пользователем
+        case 1:
+            answer = ''
+            file_to_write = open("Bebralavin.txt", "w", encoding="utf-8")
+            text = input("Введите текст: ").upper()  # Ввод исходного сообщения
+            offset = int(input("Введите сдвиг: "))
+            iterate_text(text, "encrypt")
+            print(answer)
+            file_to_write.write("Шифровка:")
+            file_to_write.write(text)
+            file_to_write.write(" - ")
+            file_to_write.write(answer)
+            file_to_write.close()
+        case 2:
+            answer = ''
+            file_to_write = open("Bebralavin.txt", "w", encoding="utf-8")
+            text = input("Введите текст: ").upper()  # Ввод исходного сообщения
+            offset = int(input("Введите сдвиг: "))
+            iterate_text(text, "decrypt")
+            print(answer)
+            file_to_write.write("Дешифровка:")
+            file_to_write.write(text)
+            file_to_write.write(" - ")
+            file_to_write.write(answer)
+            file_to_write.close()
+        case 3:
+            answer = ''
+            file_to_write = open("Bebralavin.txt", "w", encoding="utf-8")
+            text = input("Введите текст: ").upper()  # Ввод исходного сообщения
+            file_to_write.write("bruteforce:")
+            file_to_write.write(text)
+            file_to_write.write(" - ")
+            for i in range(1, 32):
+                iterate_text(text, "bruteforce")
+                print(f"Сдвиг {i}: {answer}")
+                file_to_write.write(f"{i} сдвиг:{answer}")
+                file_to_write.write(", ")
+                answer = ""
+            file_to_write.close()
+        case 4:
+            ftp = FTP()
+            ftp.connect(HOST, PORT)
+            ftp.login(USER, PASSWORD)
+            file_name = input("Какой файл отправить?\n")
+            ftp.storbinary('STOR ' + file_name, open(file_name, "rb"))  # rb - Чтение двоичного файла
+            ftp.quit()
+        case 5:
+            ftp = FTP()
+            ftp.connect(HOST, PORT)
+            ftp.login(USER, PASSWORD)
+            ftp.retrlines('LIST')
+            file_name = input("Выберите файл для скачивания:")
+            my_file = open(file_name, 'wb')  # wb - Запись двоичного файла
+            ftp.retrbinary('RETR ' + file_name, my_file.write, 1024)
+            ftp.quit()
+            my_file.close()
+        case 6:
+            ftp = FTP()
+            ftp.connect(HOST, PORT)
+            ftp.login(USER, PASSWORD)
+            ftp.retrlines('LIST')
+        case _:  # Защита от дурака
+            print("Неверный режим работы")
+
+
+    mode_choice = input("Выберите режим работы:\n"  # Ввод режима работы
+                            "1 - Шифрование\n"
+                            "2 - Дешифрование\n"
+                            "3 - Bruteforce\n"
+                            "4 - Отправить файл\n"
+                            "5 - Скачать файл\n"
+                            "6 - Список файлов\n"
+                            "7 - Выход\n")
+    mode_choice = int(mode_choice)
