2fb129685b9056a140a53c5099f423e4e8e5bf40
Bormotoon <bormotoon@gmail.com>
2023-03-14 09:32:44 +0300

    CLasses finished. Neural networking homework for 20.03.2023 init.

--- AI/classes.py
+++ AI/classes.py
@@ -1,35 +1,35 @@
-class Human:  # Создаем новый класс
-    def __init__(self, name):  # Конструктор
-        print(f"Родился человек с именем {name}!")  # Сообщение о создании новой сущности
-        self.__name = name  # ИНКАПСУЛИРОВАННЫЕ (__) свойства
-        self.__age = 0
-        self.__health = 100
-        self.__height = 35
+class Human:
+    def __init__(self, name):
+        self.__name = name
 
-    @property  # Начало GETTER (получатель/возвращатель) СВОЙСТВА
-    def age(self):
-        return self.__age
+    @property
+    def name(self):
+        return self.__name
 
-    @age.setter  # Начало SETTER ("установитель"/определитель) СВОЙСТВА
-    def age(self, new_age):
-        if 1 < new_age < 100:
-            self.__age = new_age
-        else:
-            print("Fuck off.")
+    @name.setter
+    def name(self, name):
+        self.__name = name
 
 
 class Student(Human):
-    def school(self):
-        self.school = 1739
-        print(1739)
+    def __init__(self, name):
+        super().__init__(name)
+        self.__grade = 0
+
+    def study(self, univer):
+        print(f"{self.name} is in {univer}")
+
+    @property
+    def grade(self):
+        return self.__grade
+
+    @grade.setter
+    def grade(self, grade):
+        self.__grade = grade
 
 
-male_1 = Human("Rick")  # Создание сущности класса Human с именем Rick
-female_1 = Human("Doro")
-"""print(male_1.age())
-male_1.set_age(15)
-print(male_1.age())"""
-
-print(male_1.age)
-male_1.age = 15
-print(male_1.age)
+student1 = Student("Rick")
+rick.study("МАИ")
+print(rick.grade)
+rick.grade += 1
+print(rick.grade)
