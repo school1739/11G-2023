7ede034f9a6b56a7d37c065044dc95217f7a292b
Максик <sharapovm505@gmail.com>
2023-03-15 23:02:44 +0300

    homework_BD

---
+++ Big Data/160323/1 - Songs.py
@@ -0,0 +1,60 @@
+# Есть список песен группы Depeche Mode со временем звучания с точностью до долей минут
+
+violator_songs = [
+    ['World in My Eyes', 4.86],
+    ['Sweetest Perfection', 4.43],
+    ['Personal Jesus', 4.56],
+    ['Halo', 4.9],
+    ['Waiting for the Night', 6.07],
+    ['Enjoy the Silence', 4.20],
+    ['Policy of Truth', 4.76],
+    ['Blue Dress', 4.29],
+    ['Clean', 5.83],
+]
+#
+# Распечатайте общее время звучания трех песен: 'Halo', 'Enjoy the Silence' и 'Clean' в формате
+#   Три песни звучат ХХХ.XX минут
+# Точность указывается в функции round(a, b)
+# где a, это число которое надо округлить, а b количество знаков после запятой
+# более подробно про функцию round смотрите в документации https://docs.python.org/3/search.html?q=round
+time_3_songs = 0
+for song, time in violator_songs:
+    if song in ('Halo', 'Enjoy the Silence','Clean'):
+        time_3_songs += time
+time_3_songs = round(time_3_songs, 2)
+print(f'Три песни звучат {time_3_songs} минут')
+
+# Есть словарь песен группы Yellow со временем звучания с точностью до долей минут
+pocket_universe_songs = {
+    'Solar Driftwood': 1.85,
+    'Celsius': 5.98,
+    'More': 6.65,
+    'On Track': 5.55,
+    'Monolith': 6.35,
+    'To the Sea': 5.77,
+    'Magnetic': 5.88,
+    'Liquid Mountain': 2.97,
+    'Pan Blue': 5.52,
+    'Resistor': 7.22,
+    'Beyond Mirrors': 5.82,
+}
+
+
+
+# Распечатайте общее время звучания трех песен: 'On Track', 'To the Sea' и 'Beyond Mirrors'
+#   А другие три песни звучат приблизительно ХХХ минут
+
+time_3_songs = 0
+time_other_songs = 0
+for song, time in pocket_universe_songs.items():
+    if song in ('On Track', 'To the Sea', 'Beyond Mirrors'):
+        time_3_songs += time
+    else:
+        time_other_songs += time
+time_3_songs = round(time_3_songs, 2)
+time_other_songs = round(time_other_songs)
+print(f'Три песни звучат {time_3_songs} минут')
+print(f'А другие три песни звучат приблизительно {time_other_songs} минут')
+
+# Обратите внимание, что делать много вычислений внутри print() - плохой стиль.
+# Лучше заранее вычислить необходимое, а затем в print(xxx, yyy, zzz)
