fd350d2ef7179a8e004c68d678a0543602d48c56
Gri427ggg <Grishayoda.ru@gmail.com>
2023-03-15 22:45:37 +0300

    garden

--- Big Data/160323/3 - Garden.py
+++ Big Data/160323/3 - Garden.py
@@ -7,16 +7,15 @@ meadow = ('клевер', 'одуванчик', 'ромашка', 'клевер'
 # создайте множество цветов, произрастающих в саду и на лугу
 # garden_set =
 # meadow_set =
-# TODO здесь ваш код
-
+garden_set = set(garden)
+meadow_set = set(meadow)
 # выведите на консоль все виды цветов
-# TODO здесь ваш код
-
+all_flowers = meadow_set | garden_set
+print(all_flowers)
 # выведите на консоль те, которые растут и там и там
-# TODO здесь ваш код
-
+all_location = meadow_set & garden_set
+print(all_location)
 # выведите на консоль те, которые растут в саду, но не растут на лугу
-# TODO здесь ваш код
-
+print(garden_set - meadow_set)
 # выведите на консоль те, которые растут на лугу, но не растут в саду
-# TODO здесь ваш код
+print(meadow_set - garden_set)
