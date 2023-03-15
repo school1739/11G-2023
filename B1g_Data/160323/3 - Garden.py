2143561b9a12809990e41f0a73bd0f35b20c76b5
Кузнецов Владимир <kuzvl2006@yandex.ru>
2023-03-15 22:49:13 +0300

    Little fix

--- B1g_Data/160323/3 - Garden.py
+++ B1g_Data/160323/3 - Garden.py
@@ -5,15 +5,13 @@ garden = ('ромашка', 'роза', 'одуванчик', 'ромашка', 
 meadow = ('клевер', 'одуванчик', 'ромашка', 'клевер', 'мак', 'одуванчик', 'ромашка')
 
 # создайте множество цветов, произрастающих в саду и на лугу
-# garden_set =
-# meadow_set =
-garden_set = ['тюльпан', 'нарциссы', 'ландыш', 'гвоздика']
-meadow_set = ['зверобой', 'подснежник', 'колокольчик']
+garden_set = set(garden)
+meadow_set = set(meadow)
 # выведите на консоль все виды цветов
-print(garden_set, meadow_set, garden, meadow)
+print(garden_set, meadow_set)
 # выведите на консоль те, которые растут и там и там
-print(garden, meadow)
+print(garden_set | meadow_set)
 # выведите на консоль те, которые растут в саду, но не растут на лугу
-print(garden_set, garden)
+print(garden_set)
 # выведите на консоль те, которые растут на лугу, но не растут в саду
-print(meadow_set, meadow)
+print(meadow_set)
