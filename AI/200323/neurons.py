5c1f009521451b9ca5783f11bc788196ab3002e0
herob <herobrineredmi21011@gmail.com>
2023-03-19 19:37:12 +0300

    todo 0

---
+++ AI/200323/neurons.py
@@ -0,0 +1,26 @@
+class MathNeuron:
+    def __init__(self, num_inputs):
+        self.weights = []
+        for i in range(num_inputs):
+            self.weights.append(1)  # инициализация веса до 1
+        self.bias = 0
+
+    def calculate_output(self, inputs):
+        sum = 0
+        for i in range(len(inputs)):
+            sum += inputs[i] * self.weights[i]
+        sum += self.bias
+        output = 1 / (1 + pow(2.71828, -sum))
+        return output
+
+    def set_weights(self, weights):
+        self.weights = weights
+
+    def set_bias(self, bias):
+        self.bias = bias
+
+    def get_weights(self):
+        return self.weights
+
+    def get_bias(self):
+        return self.bias
