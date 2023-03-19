11eea522d68a20fb5eb3f2a4dbd310871bcb1b40
Женя Е <janeeg56@yandex.ru>
2023-03-19 23:47:05 +0300

    2

--- AI/200323/neurons.py
+++ AI/200323/neurons.py
@@ -29,6 +29,56 @@ class MathNeuron:
 
 #  1: Написать три подкласса -- под одному для S, A, R элементов (учитываем особенности! --> ref)
 #  2: Написать универсальный класс для нейронного слоя:
+class ElemS(MathNeuron):
+    def __init__(self, nomer__inputs, threshold):
+        super().__init__(nomer__inputs)
+        self.threshold = threshold
+
+    def calcoutput(self, inputs):
+        OBSHsumma = 0
+        for i in range(len(inputs)):
+            OBSHsumma += inputs[i] * self.weights[i]
+        OBSHsumma += self.bias
+        if OBSHsumma >= self.threshold:
+            output = 1
+        else:
+            output = 0
+        return output
+
+
+class ElemA(MathNeuron):
+    def __init__(self, num_inputs, threshold):
+        super().__init__(num_inputs)
+        self.threshold = threshold
+
+    def calculate_output(self, inputs):
+        OBSHsumma = 0
+        for i in range(len(inputs)):
+            OBSHsumma += inputs[i] * self.weights[i]
+        OBSHsumma += self.bias
+        if OBSHsumma >= self.threshold:
+            output = 1
+        else:
+            output = 0
+        return output
+
+
+class ElemR(MathNeuron):
+    def __init__(self, nomer__inputs):
+        super().__init__(nomer__inputs)
+
+    def calcoutput(self, __inputs):
+        OBSHsumma = 0
+        for i in range(len(__inputs)):
+            OBSHsumma += __inputs[i] * self.weights[i]
+        OBSHsumma += self.bias
+        if OBSHsumma > 0:
+            output = 1
+        elif OBSHsumma < 0:
+            output = -1
+        else:
+            output = 0  # undefined can be represented as 0 or None
+        return output
         # Args:
         # Neuron type
         # Neuron number
