4de5e21f402761f6c0301924a4376fd90b30daa9
herob <herobrineredmi21011@gmail.com>
2023-03-19 20:25:12 +0300

    todo note 0

--- AI/200323/neurons.py
+++ AI/200323/neurons.py
@@ -25,21 +25,6 @@ class MathNeuron:
     def get_bias(self):
         return self.bias
 
-class SElement(MathNeuron):
-    def __init__(self, num_inputs, threshold):
-        super().__init__(num_inputs)
-        self.threshold = threshold
-
-    def calculate_output(self, inputs):
-        sum = 0
-        for i in range(len(inputs)):
-            sum += inputs[i] * self.weights[i]
-        sum += self.bias
-        if sum >= self.threshold:
-            output = 1
-        else:
-            output = 0
-        return output
 
 class SElement(MathNeuron):
     def __init__(self, num_inputs, threshold):
@@ -57,6 +42,24 @@ class SElement(MathNeuron):
             output = 0
         return output
 
+
+class AElement(MathNeuron):
+    def __init__(self, num_inputs, threshold):
+        super().__init__(num_inputs)
+        self.threshold = threshold
+
+    def calculate_output(self, inputs):
+        sum = 0
+        for i in range(len(inputs)):
+            sum += inputs[i] * self.weights[i]
+        sum += self.bias
+        if sum >= self.threshold:
+            output = 1
+        else:
+            output = 0
+        return output
+
+
 class RElement(MathNeuron):
     def __init__(self, num_inputs):
         super().__init__(num_inputs)
@@ -73,23 +76,3 @@ class RElement(MathNeuron):
         else:
             output = 0
         return output
-
-class RElement(MathNeuron):
-    def __init__(self, num_inputs):
-        super().__init__(num_inputs)
-
-    def calculate_output(self, inputs):
-        sum = 0
-        for i in range(len(inputs)):
-            sum += inputs[i] * self.weights[i]
-        sum += self.bias
-        if sum > 0:
-            output = 1
-        elif sum < 0:
-            output = -1
-        else:
-            output = 0
-        return output
-
-
-
