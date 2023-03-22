1067671f4d2f32b960c8f9857094e017ffcb89b3
Георгий Лыгач <glugach3@gmail.com>
2023-03-22 23:06:23 +0300

    matrix_minor 22/03/2023

--- BD/Matrix.py
+++ BD/Matrix.py
@@ -1,30 +1,47 @@
-'''the_matrix=[]
-str1 = []
-x = int(input())
-y = int(input())
-if x!=y:
-    print("-")
-else:
-    for rol in range(y):
-        the_matrix.append(str1)
-    for kola in range(x):
-        str1.append(int(input()))
-
-print(the_matrix)'''
-
-A = [[1, 2, 3, 4],
-     [5, 6, 7, 8],
-     [9, 10, 11, 12],
-     [13, 14, 15, 16]]
-def minor():
-    b = A[0].pop(0)
-    b1 = A[1].pop(0)
-    b2 = A[2].pop(0)
-    b3 = A[3].pop(0)
-    print(A)
+#создание матрицы(можно выбирать размер)
+the_matrix=[]
+p = 0
+size = 3 #int(input(f"размер матрицы: "))
+for x in range(size):
+    spx=[]
+    for sp in range(size):
+        p += 1
+        k = int(input(f"значение {p}: "))
+        spx.append(k)
+    the_matrix.append(spx)
 
 
 
-print(A)
-minor()
 
+
+zer_ind = 0  # Переменная для сохранения индекса строки с макс. кол-вом нулей
+
+
+def minor(a, del_el):
+    from copy import deepcopy
+    minor = deepcopy(a)  # Создаем независимую копию матрицы
+    global zer_ind
+    # Запоминаем индекс строки с макс. кол-вом нулей
+    for count_zer in minor:
+        if count_zer.count(0) > minor[zer_ind].count(0):
+            zer_ind = minor.index(count_zer)
+        else:
+            continue
+    # Удаляем строку с макс. кол-вом нулей
+    del minor[zer_ind]
+    # Итерируемся по строкам
+    for stroke in minor:
+        del stroke[del_el]  # Удаляем элемент нужного столбца в каждой строке
+    # Вычисление полученного минора
+    print(minor)
+    print(minor[0][0] * minor[1][1] - minor[0][1] * minor[1][0])
+    el = 0
+    a = 1
+    for repeat in range(len(minor)):
+        for stroke in minor:
+            a *= stroke[el]
+            if el == len(stroke):
+                el = 0
+            else:
+                el += 1
+    return minor
