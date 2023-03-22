a58d3f04387c36219f8c10ccb423215fef4d3c94
Кузнецов Владимир <kuzvl2006@yandex.ru>
2023-03-22 18:00:01 +0300

    BD TODO 1

--- AI/200323/neurons.py
+++ AI/200323/neurons.py
@@ -1,4 +1,30 @@
-# TODO 0: Написать класс для математического нейрона (считаем, что число выходов может быть >1, дендриты не учитываем)
+import random
+
+
+class Math_Neuron:
+    def __init__(self, x_count, w, theta):
+        self.x_count = x_count
+        self.w = w + random.random(-0.25, 0.25)
+        self.theta = theta
+        self.sum = 0
+        print(f"Создан нейрон с количеством входов {self.x_count}, весом {self.w} и порогом {self.theta}")
+
+    def get_info(self):
+        print(f"Нейрон с количеством входов {self.x_count}, весом {self.w} и порогом {self.theta}")
+
+    def activate(self, x):
+        if len(x) != self.x_count:
+            print("Количество входов не соответствует количеству входов нейрона")
+        else:
+            self.sum = 0
+            for i in range(self.x_count):
+                self.sum += x[i] * self.w[i]
+            if self.sum > self.theta:
+                print("Нейрон активирован")
+                return True
+            else:
+                print("Нейрон не активирован")
+                return False
 # TODO 1: Написать три подкласса -- под одному для S, A, R элементов (учитываем особенности! --> ref)
 # TODO 2: Написать универсальный класс для нейронного слоя:
         # Args:
---
+++ Big Data/BDmain.py
@@ -0,0 +1,16 @@
+# TODO 2: if shape 1 or 2 --> Hardcode calculation
+# TODO 3: def minor(a)
+# TODO 4: def calculate_det(matrix) using minor(a)
+# TODO 5: calculate_det(matrix)
+a11 = int(input())
+a12 = int(input())
+a13 = int(input())
+a21 = int(input())
+a22 = int(input())
+a23 = int(input())
+a31 = int(input())
+a32 = int(input())
+a33 = int(input())
+the_matrix = [[a11, a12, a13],
+              [a21, a22, a23],
+              [a31, a32, a33]]
