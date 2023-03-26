197e9d008e01823df945d6494f0a44410280dfca
Кузнецов Владимир <kuzvl2006@yandex.ru>
2023-03-26 22:05:37 +0300

    Damn neurons

--- AI/200323/neurons.py
+++ AI/200323/neurons.py
@@ -1,10 +1,10 @@
 import random
 
 
-class Math_Neuron:
+class MathNeuron:
     def __init__(self, x_count, w, theta):
         self.x_count = x_count
-        self.w = w + random.random(-0.25, 0.25)
+        self.w = w + random.choice([-0.25, 0.25])
         self.theta = theta
         self.sum = 0
         print(f"Создан нейрон с количеством входов {self.x_count}, весом {self.w} и порогом {self.theta}")
@@ -25,22 +25,94 @@ class Math_Neuron:
             else:
                 print("Нейрон не активирован")
                 return False
-# TODO 1: Написать три подкласса -- под одному для S, A, R элементов (учитываем особенности! --> ref)
-# TODO 2: Написать универсальный класс для нейронного слоя:
-        # Args:
-        # Neuron type
-        # Neuron number
-        # x
-# TODO 3: Написать функцию создания несвязной нейронной сети:
-        # Функция создаёт нужное количество слоёв нейронов по правилу: количество нейронов в первом слоё задаётся явно,
-            # второй слой -- вдвое больше первого, каждый последующий -- вдвое меньше предыдущего (количество нейронов
-            # округляем до чётных вверх); слои создаются в нужном количестве, пока в слое не останется один нейрон;
-            # типы (S, A, R) в слоях определяются автоматически.
-        # Args:
-        # Layers number
-        # 1st layer neurons number
 
-# TODO Note 0: wₙ = x ± 0.25, θₙ = 1
-# TODO Note 1: x - class arg
+
+class SNeuron(MathNeuron):
+    def __init__(self):
+        super().__init__(1, 1, 1)
+        print("Создан нейрон S")
+
+    def activate(self, x):
+        if x > self.theta:
+            print("Нейрон S активирован")
+            return True
+        else:
+            print("Нейрон S не активирован")
+            return False
+
+
+class ANeuron(MathNeuron):
+    def __init__(self, x_count, w, theta):
+        super().__init__(x_count, w, theta)
+        print("Создан нейрон A")
+
+    def activate(self, x):
+        if self.x_count == 0:
+            print("Нет входов")
+            return None
+        else:
+            if super().activate(x):
+                return True
+            else:
+                return False
+
+
+class RNeuron(MathNeuron):
+    def __init__(self, x_count, w, theta):
+        super().__init__(x_count, w, theta)
+        print("Создан нейрон R")
+
+    def activate(self):
+        if self.sum > self.theta:
+            print("Нейрон R активирован")
+            return 1
+        elif self.sum == self.theta:
+            print("Нейрон R не определён")
+            return None
+        else:
+            print("Нейрон R не активирован")
+            return -1
+
+
+class Layer:
+    def __init__(self, type, number, x):
+        self.type = type
+        self.number = number
+        self.x = x
+        self.neurons = []
+        self.create_neurons()
+
+    def create_neurons(self):
+        if self.type == "S":
+            for i in range(self.number):
+                self.neurons.append(SNeuron())
+        elif self.type == "A":
+            for i in range(self.number):
+                self.neurons.append(ANeuron(self.x, 1, 1))
+        elif self.type == "R":
+            for i in range(self.number):
+                self.neurons.append(RNeuron(self.x, 1, 1))
+        else:
+            print("Неверный тип нейрона")
+            return None
+
+
+def Layer_creater(neurons_number):
+    layers = []
+    past_neurons_number = neurons_number
+    neurons_number = neurons_number * 2
+    while neurons_number != 1:
+        layers.append(Layer('S', neurons_number, past_neurons_number))
+        layers.append(Layer('A', neurons_number, past_neurons_number))
+        layers.append(Layer('R', neurons_number, past_neurons_number))
+        past_neurons_number = neurons_number
+        neurons_number = neurons_number // 2
+        if neurons_number % 2 != 0 and neurons_number != 1:
+            neurons_number = neurons_number + 1
+    layers.append(Layer('R', 1, 2))
+
 
 # Reference: http://bit.ly/3FmnVEo
+
+
+Layer_creater(10)
