b0b891c6a9180248f624a4598db5fd2e15b3cb47
gorbatenkovdv <nnerf54@gmail.com>
2023-03-26 19:53:22 +0300

    2 todo

--- AI/200323/neurons.py
+++ AI/200323/neurons.py
@@ -25,3 +25,48 @@ class MathNeuron:
             else:
                 print("Нейрон не активирован")
                 return False
+lass SNeuron(MathNeuron):
+    def __init__(self):
+        super().__init__(1, 1, 1)
+        print("Создан нейрон S")
+
+    def activate(self, x):
+        if x > self.theta:
+            print("Нейрон S активирован")
+            return True
+        else:
+            print("Нейрон S не активирован")
+            return False
+
+
+class ANeuron(MathNeuron):
+    def __init__(self, x_count, w, theta):
+        super().__init__(x_count, w, theta)
+        print("Создан нейрон A")
+
+    def activate(self, x):
+        if self.x_count == 0:
+            print("Нет входов")
+            return None
+        else:
+            if super().activate(x):
+                return True
+            else:
+                return False
+
+
+class RNeuron(MathNeuron):
+    def __init__(self, x_count, w, theta):
+        super().__init__(x_count, w, theta)
+        print("Создан нейрон R")
+
+    def activate(self):
+        if self.sum > self.theta:
+            print("Нейрон R активирован")
+            return 1
+        elif self.sum == self.theta:
+            print("Нейрон R не определён")
+            return None
+        else:
+            print("Нейрон R не активирован")
+            return -1
