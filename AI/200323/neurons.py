da59f53c833eb54f52463de3223a7558310b1fae
Женя Е <janeeg56@yandex.ru>
2023-03-26 15:00:05 +0300

    функция создания слоев

--- AI/200323/neurons.py
+++ AI/200323/neurons.py
@@ -1,88 +1,103 @@
-    #  0: Написать класс для математического нейрона (считаем, что число выходов может быть >1, дендриты не учитываем)
+import random
+
+
 class MathNeuron:
-    def __init__(self, nomerinputs):
-        self.weights = []
-        for i in range(nomerinputs):
-            self.weights.append(1)
-        self.bias = 0
+    def __init__(self, x_count, w, theta):
+        self.x_count = x_count
+        self.w = w + random.random(-0.25, 0.25)
+        self.theta = theta
+        self.sum = 0
+        print(f"Создан нейрон с количеством входов {self.x_count}, весом {self.w} и порогом {self.theta}")
 
-    def calcoutputs(self, inputs):
-        OBSHsumma = 0
-        for i in range(len(inputs)):
-            OBSHsumma += inputs[i] * self.weights[i]
-        OBSHsumma += self.bias
-        output = 1 / (1 + pow(2.71828, -OBSHsumma))
-        return output
+    def get_info(self):
+        print(f"Нейрон с количеством входов {self.x_count}, весом {self.w} и порогом {self.theta}")
 
-    def setw(self, weights):
-        self.weights = weights
-
-    def setb(self, bias):
-        self.bias = bias
-
-    def getw(self):
-        return self.weights
-
-
-    def getb(self):
-        return self.bias
-
-#  1: Написать три подкласса -- под одному для S, A, R элементов (учитываем особенности! --> ref)
-#  2: Написать универсальный класс для нейронного слоя:
-class ElemS(MathNeuron):
-    def __init__(self, nomer__inputs, threshold):
-        super().__init__(nomer__inputs)
-        self.threshold = threshold
-
-    def calcoutput(self, inputs):
-        OBSHsumma = 0
-        for i in range(len(inputs)):
-            OBSHsumma += inputs[i] * self.weights[i]
-        OBSHsumma += self.bias
-        if OBSHsumma >= self.threshold:
-            output = 1
+    def activate(self, x):
+        if len(x) != self.x_count:
+            print("Количество входов не соответствует количеству входов нейрона")
         else:
-            output = 0
-        return output
+            self.sum = 0
+            for i in range(self.x_count):
+                self.sum += x[i] * self.w[i]
+            if self.sum > self.theta:
+                print("Нейрон активирован")
+                return True
+            else:
+                print("Нейрон не активирован")
+                return False
 
 
-class ElemA(MathNeuron):
-    def __init__(self, num_inputs, threshold):
-        super().__init__(num_inputs)
-        self.threshold = threshold
+class SNeuron(MathNeuron):
+    def __init__(self):
+        super().__init__(1, 1, 1)
+        print("Создан нейрон S")
 
-    def calculate_output(self, inputs):
-        OBSHsumma = 0
-        for i in range(len(inputs)):
-            OBSHsumma += inputs[i] * self.weights[i]
-        OBSHsumma += self.bias
-        if OBSHsumma >= self.threshold:
-            output = 1
+    def activate(self, x):
+        if x > self.theta:
+            print("Нейрон S активирован")
+            return True
         else:
-            output = 0
-        return output
+            print("Нейрон S не активирован")
+            return False
 
 
-class ElemR(MathNeuron):
-    def __init__(self, nomer__inputs):
-        super().__init__(nomer__inputs)
+class ANeuron(MathNeuron):
+    def __init__(self, x_count, w, theta):
+        super().__init__(x_count, w, theta)
+        print("Создан нейрон A")
 
-    def calcoutput(self, __inputs):
-        OBSHsumma = 0
-        for i in range(len(__inputs)):
-            OBSHsumma += __inputs[i] * self.weights[i]
-        OBSHsumma += self.bias
-        if OBSHsumma > 0:
-            output = 1
-        elif OBSHsumma < 0:
-            output = -1
+    def activate(self, x):
+        if self.x_count == 0:
+            print("Нет входов")
+            return None
         else:
-            output = 0  # undefined can be represented as 0 or None
-        return output
-        # Args:
-        # Neuron type
-        # Neuron number
-        # x
+            if super().activate(x):
+                return True
+            else:
+                return False
+
+
+class RNeuron(MathNeuron):
+    def __init__(self, x_count, w, theta):
+        super().__init__(x_count, w, theta)
+        print("Создан нейрон R")
+
+    def activate(self):
+        if self.sum > self.theta:
+            print("Нейрон R активирован")
+            return 1
+        elif self.sum == self.theta:
+            print("Нейрон R не определён")
+            return None
+        else:
+            print("Нейрон R не активирован")
+            return -1
+
+
+class Layer:
+    def __init__(self, type, number, x):
+        self.type = type
+        self.number = number
+        self.x = x
+        self.neurons = []
+        self.create_neurons()
+
+    def create_neurons(self):
+        if self.type == "S":
+            for i in range(self.number):
+                self.neurons.append(SNeuron())
+        elif self.type == "A":
+            for i in range(self.number):
+                self.neurons.append(ANeuron(self.x, 1, 1))
+        elif self.type == "R":
+            for i in range(self.number):
+                self.neurons.append(RNeuron(self.x, 1, 1))
+        else:
+            print("Неверный тип нейрона")
+            return None
+
+
+
 #  3: Написать функцию создания несвязной нейронной сети:
         # Функция создаёт нужное количество слоёв нейронов по правилу: количество нейронов в первом слоё задаётся явно,
             # второй слой -- вдвое больше первого, каждый последующий -- вдвое меньше предыдущего (количество нейронов
@@ -91,62 +106,37 @@ class ElemR(MathNeuron):
         # Args:
         # Layers number
         # 1st layer neurons number
-class Neironal:
-    def __init__(self, tips_neiron, nomer_neuronov, x = None):
-        self.neirons = []
-        for i in range(nomer_neuronov):
-            if x is not None:
-                neiron1 = tips_neiron(x)
-            else:
-                neiron1 = tips_neiron()
-            self.neurons.append(neiron1)
-
-    def compoutputs(self, inputs):
-        outputs = []
-        for neiron1 in self.neurons:
-            outputs.append(neiron1.calcoutput(inputs))
-        return outputs
-
-    def setw(self, weights):
-        for neuron, weight in zip(self.neirons, weights):
-            neuron.setw(weight)
-
-    def setb(self, biases):
-        for neiron1, bias in zip(self.neurons, biases):
-            neiron1.setb(bias)
-
-    def getter_neirons(self):
-        return self.neirons
 
 
 
-def necconnectir_neiron(nomer_l, neirons_l1):
-    innwor = []
-    neirons_curr = neirons_l1
-
-    for i in range(nomer_l):
-        __layerNN = []
-
-        for j in range(neirons_curr):
-            if j == 0:
-                __layerNN.append('S')
-            elif j % 2 == 0:
-                __layerNN.append('A')
-            else:
-                __layerNN.append('R')
-
-        innwor.append(__layerNN)
-        current_layer_neurons = (neirons_curr // 2) + (neirons_curr % 2)
-
-    innwor.append(['S'])
-
-    return innwor
 
 
-    #  Note 0: wₙ = x ± 0.25, θₙ = 1
-#  Note 1: x - class arg
+def create_network(kol_vo_neir1s, kol_vo_trebsl):#kol_vo_neir1s - количество нейронов в первом слое, задаваемое явно  #kol_vo_trebsl - количество требуемых слоев в сети
+    network = []# будет хранить все слои
+    second_sloi_neiron = kol_vo_neir1s * 2
+    network.append([None] * kol_vo_neir1s)  # добавляем в конец списка нужное кол-во элементов
+    network.append(second_sloi_neiron * [None])
+    if second_sloi_neiron  % 2 == 0:# вроде как округление до четных вверъх
+        second_sloi_neiron = second_sloi_neiron
+    else:
+        second_sloi_neiron = second_sloi_neiron + 1
+    for i in range(1, kol_vo_trebsl):
+        cur_neurons = max(1, second_sloi_neiron //2 )# max для того чтобы если кто-то додумается ввести отрицательное количество слоев, то выводилось бы 1
+        network.append([None] * cur_neurons)
+        second_sloi_neiron = cur_neurons # обновляем кол-во нейронов для следущего слоя
+    return network # и возвращаем весь список слоев сети
 
-# Reference: http://bit.ly/3FmnVEo
+#network = create_network(7, -4) # пример выполнения c отрицательным количеством слоев(проверка на дурака)
+network = create_network(5, 3)# обычный пример для слоев
+print(network)
+
+
+# По идее сейчас работает как надо,  это уже измененная версия
+
+
+
+
+
 
 
 
