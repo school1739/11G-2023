159a97d8e9a43b3fa6296851238861c3dc77717d
gorbatenkovdv <nnerf54@gmail.com>
2023-03-26 19:49:56 +0300

    1 todo

--- AI/200323/neurons.py
+++ AI/200323/neurons.py
@@ -1,131 +1,27 @@
+import random
+
+
 class MathNeuron:
-    def __init__(self, num_inputs):
-        self.weights = []
-        for i in range(num_inputs):
-            self.weights.append(1)
-        self.bias = 0
+    def __init__(self, x_count, w, theta):
+        self.x_count = x_count
+        self.w = w + round(random.triangular(-0.25, 0.25), 2)
+        self.theta = theta
+        self.sum = 0
+        print(f"Создан нейрон с количеством входов {self.x_count}, весом {self.w} и порогом {self.theta}")
 
-    def calculate_output(self, inputs):
-        sum = 0
-        for i in range(len(inputs)):
-            sum += inputs[i] * self.weights[i]
-        sum += self.bias
-        output = 1 / (1 + pow(2.71828, -sum))
-        return output
+    def get_info(self):
+        print(f"Нейрон с количеством входов {self.x_count}, весом {self.w} и порогом {self.theta}")
 
-    def set_weights(self, weights):
-        self.weights = weights
-
-    def set_bias(self, bias):
-        self.bias = bias
-
-    def get_weights(self):
-        return self.weights
-
-
-    def get_bias(self):
-        return self.bias
-
-
-class SElement(MathNeuron):
-    def __init__(self, num_inputs, threshold):
-        super().__init__(num_inputs)
-        self.threshold = threshold
-
-    def calculate_output(self, inputs):
-        sum = 0
-        for i in range(len(inputs)):
-            sum += inputs[i] * self.weights[i]
-        sum += self.bias
-        if sum >= self.threshold:
-            output = 1
+    def activate(self, x):
+        if len(x) != self.x_count:
+            print("Количество входов не соответствует количеству входов нейрона")
         else:
-            output = 0
-        return output
-
-
-class AElement(MathNeuron):
-    def __init__(self, num_inputs, threshold):
-        super().__init__(num_inputs)
-        self.threshold = threshold
-
-    def calculate_output(self, inputs):
-        sum = 0
-        for i in range(len(inputs)):
-            sum += inputs[i] * self.weights[i]
-        sum += self.bias
-        if sum >= self.threshold:
-            output = 1
-        else:
-            output = 0
-        return output
-
-
-class RElement(MathNeuron):
-    def __init__(self, num_inputs):
-        super().__init__(num_inputs)
-
-    def calculate_output(self, inputs):
-        sum = 0
-        for i in range(len(inputs)):
-            sum += inputs[i] * self.weights[i]
-        sum += self.bias
-        if sum > 0:
-            output = 1
-        elif sum < 0:
-            output = -1
-        else:
-            output = 0  # undefined can be represented as 0 or None
-        return output
-class NeuralLayer:
-    def __init__(self, neuron_type, num_neurons, x=None):
-        self.neurons = []
-        for i in range(num_neurons):
-            if x is not None:
-                neuron = neuron_type(x)
+            self.sum = 0
+            for i in range(self.x_count):
+                self.sum += x[i] * self.w[i]
+            if self.sum > self.theta:
+                print("Нейрон активирован")
+                return True
             else:
-                neuron = neuron_type()
-            self.neurons.append(neuron)
-
-    def compute_outputs(self, inputs):
-        outputs = []
-        for neuron in self.neurons:
-            outputs.append(neuron.calculate_output(inputs))
-        return outputs
-
-    def set_weights(self, weights):
-        for neuron, weight in zip(self.neurons, weights):
-            neuron.set_weights(weight)
-
-    def set_bias(self, biases):
-        for neuron, bias in zip(self.neurons, biases):
-            neuron.set_bias(bias)
-
-    def get_neurons(self):
-        return self.neurons
-def create_disconnected_nn(layer_num, first_layer_neurons):
-    network = []
-    current_layer_neurons = first_layer_neurons
-
-    for i in range(layer_num):
-        layer = []
-
-        for j in range(current_layer_neurons):
-            if j == 0:
-                layer.append('S')
-            elif j % 2 == 0:
-                layer.append('A')
-            else:
-                layer.append('R')
-
-        network.append(layer)
-        current_layer_neurons = (current_layer_neurons // 2) + (current_layer_neurons % 2)
-
-    network.append(['S'])
-
-    return network
-
-# TODO Note 0: wₙ = x ± 0.25, θₙ = 1
-# TODO Note 1: x - class arg
-
-# Reference: http://bit.ly/3FmnVEo
+                print("Нейрон не активирован")
+                return False
