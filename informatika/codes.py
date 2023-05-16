0bfcc4f68e0cd882c2e80c0bbc4eb92d71f6ae0b
Student <sdgas@gmail.com>
2023-05-16 11:12:35 +0300

    26

--- informatika/codes.py
+++ informatika/codes.py
@@ -107,3 +107,15 @@ while n < 1000:
         pass
     n += 1'''
 
+# кегэ 2686
+f = open('26_2686.txt')
+k = int(f.readline())
+a = sorted([list(map(int, x.split())) for x in f])
+count = 0
+for i in range(k-1):
+    if a[i][0] == a[i + 1][0] and a[i][1] + 1 == a[i+1][1]:
+        count += 1
+        if count >= 5:
+            print(a[i][0], a[i][1] + 1)
+    else:
+        count = 0
