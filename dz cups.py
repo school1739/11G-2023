500ea55a61e0f61b322776023fa48a1605f6ff4b
mavis <ookok@>
2023-05-23 13:07:12 +0300

    дз ии

--- dz cups.py
+++ dz cups.py
@@ -1,8 +1,7 @@
 import random
 
-ch = 0
-ai = 0
-the_glasses = {1: [1, 1, 1, 2, 2, 2],
+
+stakanchiki = {1: [1, 1, 1, 2, 2, 2],
                2: [1, 1, 1, 2, 2, 2],
                3: [1, 1, 1, 2, 2, 2],
                4: [1, 1, 1, 2, 2, 2],
@@ -13,67 +12,99 @@ the_glasses = {1: [1, 1, 1, 2, 2, 2],
                9: [1, 1, 1, 2, 2, 2],
                10: [1, 1, 1, 2, 2, 2],
                11: [1, 1, 1, 2, 2, 2]}
-
-used_glasses = {}
-situation = 11
-ai_is_winner = False
-
+Ick_int_points = 0
+pols_points = 0
+count_rounds = 1
 
 def move(situation):
-    print("Ходит ИИ:")
-    if situation == 1:
-        the_move = 1
+    if situation != 1:
+        the_move = random.choice(stakanchiki[situation])
     else:
-        the_move = random.choice(the_glasses[situation])
-    print(f"ИИ взял {the_move} палочек.")
+        the_move = 1
+
+    print('Ходит ИИ:')
+    if the_move == 1:
+        print(f'ИИ взял 1 палочку')
+    else:
+        print(f'ИИ взял {the_move} палочек')
     used_glasses.update({situation: the_move})
     return the_move
 
 
 def used_glass_fill():
-    pass
+    if Ick_int_winner:
+        for i in used_glasses:
+            stakanchiki[i].append(used_glasses[i])
+    else:
+        for i in used_glasses:
+            if used_glasses[i] in stakanchiki[i]:
+                stakanchiki[i].remove(used_glasses[i])
 
 
-while ch < 10 and ai < 10:
+while Ick_int_points != 10 and pols_points != 10:
+    Ick_int_winner = False
     situation = 11
-    print(f'Счет: {ch} : {ai}')
+    used_glasses = {}
+    print('---------------------------')
+    print(f'Счёт: Пользователь {pols_points}:{Ick_int_points} ИИ')
+    print(f'{count_rounds}-й раунд!')
     print(f'На столе {situation} палочек')
+    print('---------------------------')
     while situation > 0:
-        if ai_is_winner == False:
+        if Ick_int_winner == False:
             if situation == 0:
                 print("Пользователь проиграл, ИИ выиграл")
-                ai += 1
-                ai_is_winner = True
+                Ick_int_winner = True
             elif situation == 1:
                 situation -= 1
                 print('Ходит пользователь')
                 print('Пользователю ничего не остаётся, кроме как взять 1 палочку')
                 print(f'На столе {situation} палочек')
                 print("Пользователь проиграл, ИИ выиграл")
-                ai += 1
-                ai_is_winner = True
+                Ick_int_winner = True
                 break
 
-        choice = int(input('Сколько палочек взять? 1 или 2?'))
-        while choice != 1 and choice != 2:
-            choice = int(input("Сказано же, одну или две! "))
-        situation = situation - choice
-        print(f'На столе {situation} палочек')
-        if situation == 0:
-            print('ИИ проиграл, пользователь выиграл')
-            ai_is_winner = False
-            ch += 1
-        else:
-            situation = situation - move(situation)
+            choice = int(input('Сколько палочек взять? 1 или 2? '))
+            while choice != 1 and choice != 2:
+                choice = int(input("Сказано же, одну или две! "))
+            situation = situation - choice
             print(f'На столе {situation} палочек')
-
             if situation == 0:
                 print('ИИ проиграл, пользователь выиграл')
-                ai_is_winner = False
-                ch += 1
-if ch == 10:
-    print('Пользователь победил')
-    print('Конец игры')
+            else:
+                situation = situation - move(situation)
+                print(f'На столе {situation} палочек')
+
+                if situation == 0:
+                    print('ИИ проиграл, пользователь выиграл')
+    count_rounds += 1
+
+    if Ick_int_winner:
+        Ick_int_points += 1
+    else:
+        pols_points += 1
+
+    used_glass_fill()
+
+print()
+print('---------------------------')
+print(f'Окончательный счёт: Пользователь {pols_points}:{Ick_int_points} ИИ')
+if Ick_int_points > pols_points:
+    print('Победа ИИ')
 else:
-    print('ИИ победил')
-    print('Конец игры')
+    print('Победил пользователь')
+print('---------------------------')
+print()
+print('Вывод содержимого стаканов')
+for number, content in stakanchiki.items():
+    print(f'{number}-й стакан: {content}')
+print('---------------------------')
+print()
+print('Вывод шанса выпадения 1 или 2 для каждого стакана')
+for number, content in stakanchiki.items():
+    try:
+        print(f'{number}-й стакан:\nШанс выпадения 1 - {round((content.count(1)/len(content))*100, 1)}%\nШанс выпадения 2 - {round((content.count(2)/len(content))*100, 1)}%')
+    except:
+        print(f'{number}-й стакан:\nШанс выпадения 1 - 0.0%\nШанс выпадения 2 - 0.0%')
+
+
