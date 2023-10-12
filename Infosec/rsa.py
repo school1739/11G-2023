65e7ffe945ddb4fb0a7329ec756bb189f3c6dcaa
Anton <Anton17_00@mail.ru>
2023-10-12 22:18:37 +0300

    лучше функция эйлера в руках, чем алгоритм евклида в небе

---
+++ Infosec/rsa.py
@@ -0,0 +1,86 @@
+# Large Prime Generation for RSA
+import random
+import math
+
+# Pre generated primes
+first_primes_list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+                     31, 37, 41, 43, 47, 53, 59, 61, 67,
+                     71, 73, 79, 83, 89, 97, 101, 103,
+                     107, 109, 113, 127, 131, 137, 139,
+                     149, 151, 157, 163, 167, 173, 179,
+                     181, 191, 193, 197, 199, 211, 223,
+                     227, 229, 233, 239, 241, 251, 257,
+                     263, 269, 271, 277, 281, 283, 293,
+                     307, 311, 313, 317, 331, 337, 347, 349]
+
+
+def nBitRandom(n):
+    return random.randrange(2**(n - 1) + 1, 2**n - 1)
+
+
+def getLowLevelPrime(n):
+    '''Generate a prime candidate divisible
+	by first primes'''
+    while True:
+        # Obtain a random number
+        pc = nBitRandom(n)
+
+        # Test divisibility by pre-generated
+        # primes
+        for divisor in first_primes_list:
+            if pc % divisor == 0 and divisor**2 <= pc:
+                break
+        else:
+            return pc
+
+
+def isMillerRabinPassed(mrc):
+    '''Run 20 iterations of Rabin Miller Primality test'''
+    maxDivisionsByTwo = 0
+    ec = mrc - 1
+    while ec % 2 == 0:
+        ec >>= 1
+        maxDivisionsByTwo += 1
+    assert (2**maxDivisionsByTwo * ec == mrc - 1)
+
+    def trialComposite(round_tester):
+        if pow(round_tester, ec, mrc) == 1:
+            return False
+        for i in range(maxDivisionsByTwo):
+            if pow(round_tester, 2**i * ec, mrc) == mrc - 1:
+                return False
+        return True
+
+    # Set number of trials here
+    numberOfRabinTrials = 20
+    for i in range(numberOfRabinTrials):
+        round_tester = random.randrange(2, mrc)
+        if trialComposite(round_tester):
+            return False
+    return True
+
+
+def get_key():
+    while True:
+        n = 1024
+        prime_candidate = getLowLevelPrime(n)
+        if not isMillerRabinPassed(prime_candidate):
+            continue
+        else:
+            return prime_candidate
+            break
+
+
+p = get_key()
+q = get_key()
+n = p * q
+fi = (p - 1) * (q - 1)
+
+
+def fe(fi):
+    s = [i for i in range(1, fi + 1) if math.gcd(fi, i) == 1]
+    e = random.choice(s)
+    return e
+
+
+
