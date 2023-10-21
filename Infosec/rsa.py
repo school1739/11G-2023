367719b94bc5ee572a9e55945f2746808d7d9245
Anton <Anton17_00@mail.ru>
2023-10-21 15:47:58 +0300

    лучше функция эйлера в руках, чем алгоритм евклида в небе

--- Infosec/rsa.py
+++ Infosec/rsa.py
@@ -1,6 +1,4 @@
-# Large Prime Generation for RSA
 import random
-import math
 
 # Pre generated primes
 first_primes_list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
@@ -15,12 +13,12 @@ first_primes_list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
 
 
 def nBitRandom(n):
-    return random.randrange(2**(n - 1) + 1, 2**n - 1)
+    return random.randrange(2 ** (n - 1) + 1, 2 ** n - 1)
 
 
 def getLowLevelPrime(n):
     '''Generate a prime candidate divisible
-	by first primes'''
+    by first primes'''
     while True:
         # Obtain a random number
         pc = nBitRandom(n)
@@ -28,7 +26,7 @@ def getLowLevelPrime(n):
         # Test divisibility by pre-generated
         # primes
         for divisor in first_primes_list:
-            if pc % divisor == 0 and divisor**2 <= pc:
+            if pc % divisor == 0 and divisor ** 2 <= pc:
                 break
         else:
             return pc
@@ -41,13 +39,13 @@ def isMillerRabinPassed(mrc):
     while ec % 2 == 0:
         ec >>= 1
         maxDivisionsByTwo += 1
-    assert (2**maxDivisionsByTwo * ec == mrc - 1)
+    assert (2 ** maxDivisionsByTwo * ec == mrc - 1)
 
     def trialComposite(round_tester):
         if pow(round_tester, ec, mrc) == 1:
             return False
         for i in range(maxDivisionsByTwo):
-            if pow(round_tester, 2**i * ec, mrc) == mrc - 1:
+            if pow(round_tester, 2 ** i * ec, mrc) == mrc - 1:
                 return False
         return True
 
@@ -59,28 +57,39 @@ def isMillerRabinPassed(mrc):
             return False
     return True
 
+def get_nod(a,b):
+    while a != b:
+        if a>b:
+            a -= b
+        else:
+            b -= a
+    return a
 
-def get_key():
+def generatePrime(m):
     while True:
-        n = 1024
-        prime_candidate = getLowLevelPrime(n)
+        prime_candidate = getLowLevelPrime(m)
         if not isMillerRabinPassed(prime_candidate):
             continue
         else:
             return prime_candidate
+
+def generateKeys():
+    p = generatePrime(129)
+    q = generatePrime(129)
+    n = p*q
+    fin = (p-1)*(q-1)
+    while True:
+        e = random.randrange(1,fin)
+        if get_nod(e,fin) != 1:
+            continue
+        else:
             break
+    d = fin * e
+    return [(e,n),(d,n)]
 
+keys = generateKeys()
 
-p = get_key()
-q = get_key()
-n = p * q
-fi = (p - 1) * (q - 1)
-
-
-def fe(fi):
-    s = [i for i in range(1, fi + 1) if math.gcd(fi, i) == 1]
-    e = random.choice(s)
-    return e
-
+public = open('publickey.txt', 'w').write(str(keys[0]))
+private = open('privatekey.txt', 'w').write(str(keys[1]))
 
 
