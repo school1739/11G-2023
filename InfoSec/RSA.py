7fc5a0fd92d0fc6df23b39ce2779d986b7f0486e
LinexForte <79544593+LinexForte@users.noreply.github.com>
2023-11-26 23:39:17 +0300

    RSA

---
+++ InfoSec/RSA.py
@@ -0,0 +1,84 @@
+import random
+
+first_primes_list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+                     31, 37, 41, 43, 47, 53, 59, 61, 67,
+                     71, 73, 79, 83, 89, 97, 101, 103,
+                     107, 109, 113, 127, 131, 137, 139,
+                     149, 151, 157, 163, 167, 173, 179,
+                     181, 191, 193, 197, 199, 211, 223,
+                     227, 229, 233, 239, 241, 251, 257,
+                     263, 269, 271, 277, 281, 283, 293,
+                     307, 311, 313, 317, 331, 337, 347, 349]
+
+
+def nBitRandom(n):
+    return random.randrange(2 ** (n - 1) + 1, 2 ** n - 1)
+
+
+def getLowLevelPrime(n):
+    while True:
+        pc = nBitRandom(n)
+
+        for divisor in first_primes_list:
+            if pc % divisor == 0 and divisor ** 2 <= pc:
+                break
+        else:
+            return pc
+
+
+def isMillerRabinPassed(mrc):
+    maxDivisionsByTwo = 0
+    ec = mrc - 1
+    while ec % 2 == 0:
+        ec >>= 1
+        maxDivisionsByTwo += 1
+    assert (2 ** maxDivisionsByTwo * ec == mrc - 1)
+    
+    def trialComposite(round_tester):
+        if pow(round_tester, ec, mrc) == 1:
+            return False
+        for i in range(maxDivisionsByTwo):
+            if pow(round_tester, 2 ** i * ec, mrc) == mrc - 1:
+                return False
+        return True
+
+    numberOfRabinTrials = 20
+    for i in range(numberOfRabinTrials):
+        round_tester = random.randrange(2, mrc)
+        if trialComposite(round_tester):
+            return False
+    return True
+
+def get_nod(a,b):
+    while a != b:
+        if a>b:
+            a -= b
+        else:
+            b -= a
+    return a
+
+def generatePrime(m):
+    while True:
+        prime_candidate = getLowLevelPrime(m)
+        if not isMillerRabinPassed(prime_candidate):
+            continue
+        else:
+            return prime_candidate
+        
+def generateKeys():
+    p = generatePrime(129)
+    q = generatePrime(129)
+    n = p*q
+    fin = (p-1)*(q-1)
+    while True:
+        e = random.randrange(1,fin)
+        if get_nod(e,fin) != 1:
+            continue
+        else:
+            break
+    d = fin * e
+    return [(e,n),(d,n)]
+
+keys = generateKeys()
+public = open('publickey.txt', 'w').write(str(keys[0]))
+private = open('privatekey.txt', 'w').write(str(keys[1]))
